a861aed7b26ee20e0b3425150c795528f9cde60e
jenniferInSH <18770810604@163.com>
2022-05-02 19:54:49 +0800

    对原始日期进行简单修改

---
+++ src/main/java/com/henry/tryout/java_8_features/date_and_time_08/manipulate_parse_and_format_date_02/Hello.java
@@ -0,0 +1,7 @@
+package com.henry.tryout.java_8_features.date_and_time_08.manipulate_parse_and_format_date_02;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Okay");
+    }
+}
