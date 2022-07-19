81301bf5c382f9b926f0b452ae2c14fac360d0ca
jenniferInSH <18770810604@163.com>
2022-07-20 00:00:27 +0800

    easy coding: introduce log framework in Maven project.

---
+++ src/main/java/com/henry/tryout/easy_coding/exception_and_log/log_04/LogFrameworkUseDemo.java
@@ -0,0 +1,13 @@
+package com.henry.tryout.easy_coding.exception_and_log.log_04;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogFrameworkUseDemo {
+    // 静态成员变量能够减少资源消耗 （因为与类绑定）
+    private static final Logger logger = LoggerFactory.getLogger(LogFrameworkUseDemo.class);
+
+    public static void main(String[] args) {
+
+    }
+}
