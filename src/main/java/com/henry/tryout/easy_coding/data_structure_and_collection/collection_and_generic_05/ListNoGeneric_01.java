62c3c25c2c3817cceb679eb0f58e10de7f3f45f2
jenniferInSH <18770810604@163.com>
2022-07-24 09:11:34 +0800

    easy coding: generic and collection.

---
+++ src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/collection_and_generic_05/ListNoGeneric_01.java
@@ -0,0 +1,46 @@
+package com.henry.tryout.easy_coding.data_structure_and_collection.collection_and_generic_05;
+
+import com.henry.tryout.easy_coding.data_structure_and_collection.collection_framework_02.ArrayList;
+
+import java.util.List;
+
+public class ListNoGeneric_01 {
+    public static void main(String[] args) {
+
+        // 没有泛型之前，定义集合的方式
+        List a1 = new ArrayList();
+        a1.add(new Object());
+        a1.add(new Integer(111));
+        a1.add(new String("hello a1a1"));
+
+        // 把a1的引用赋值给a2 - a2添加了泛型限制 <Object>
+        List<Object> a2 = a1;
+        a2.add(new Object());
+        a2.add(new Integer(222));
+        a2.add(new String("hello a2a2"));
+
+        // #3 把a1引用 赋值给a3 - a3添加了泛型限制 <Integer>
+        List<Integer> a3 = a1;
+        a3.add(new Integer(333));
+        // 添加了泛型限制后，不再允许添加 非Integer类型的item进入集合
+//        a3.add(new Object());
+//        a3.add(new String("hello a3a3"));
+
+        // 把a1的引用赋值给a4 - a4添加了 通配符的泛型限制
+        List<?> a4 = a1;
+        // 允许删除和清空元素
+        a1.remove(0);
+        a4.clear();
+        // 但不允许添加任何元素
+//        a4.add(new Object());
+    }
+}
+/*
+启示：
+     1 ?在正则表达式中可以匹配任何字符；
+     2 List<?> 类型变量 能够接受任何类型的集合引用；
+     3 List<?> 变量，可以不能添加元素，但可以remove() 与 clear()。
+     应用：
+        1 作为参数 接收外部传入的集合；
+        2 返回一个 不知道元素类型具体是什么的集合。
+ */
---
+++ src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/collection_and_generic_05/bindingNoGenericToGeneric_02.java
@@ -0,0 +1,33 @@
+package com.henry.tryout.easy_coding.data_structure_and_collection.collection_and_generic_05;
+
+import net.sf.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class bindingNoGenericToGeneric_02 {
+    public static void main(String[] args) {
+        JSONObject jsonObject = JSONObject.fromObject("{\"level\": [\"3\"]}");
+        List<Integer> intList = new ArrayList<>(10);
+
+        if (jsonObject != null) {
+            // 这里把解析出来的 字符串3 添加到了 intList列表中
+            // 特征： 编译时不报错，但运行时报错 - java.lang.String cannot be cast to java.lang.Integer
+            // 原因：  getJSONArray()方法的返回值类型JSONArray 是List的子类 & 非泛型集合可以赋值给任何 泛型集合
+            intList.addAll(jsonObject.getJSONArray("level"));
+
+            int amount = 0;
+            for (Integer x : intList) {
+                if (x < 10) {
+                    amount = amount + 1;
+                }
+            }
+        }
+    }
+}
+/*
+结论：
+    1 非泛型集合 可以赋值给任何 泛型集合；
+    2 由于JDK在5之后才引入泛型，所以 可能会出现 非泛型赋值给泛型 的代码；
+    3 在JDK5之后，应该尽量使用泛型定义。
+ */
