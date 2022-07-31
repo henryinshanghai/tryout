8280a5309d15f80dafb1b9dd7d52dcba23f9ff85
jenniferInSH <18770810604@163.com>
2022-07-31 22:19:44 +0800

    easy coding - HashMap JDK1.7的实现，会导致高并发条件下，发生对象丢失 与 死链(链表中出现环)的情况。

---
+++ src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/map_08/hashMap_03/HashMapEndlessLoop.java
@@ -0,0 +1,19 @@
+package com.henry.tryout.easy_coding.data_structure_and_collection.map_08.hashMap_03;
+
+import java.util.HashMap;
+
+public class HashMapEndlessLoop {
+    private static HashMap<Long, EasyCoding> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100000; i++) {
+            (new Thread(){
+                public void run() {
+                    map.put(System.nanoTime(), new EasyCoding());
+                }
+            }).start();
+
+        }
+    }
+}
+
