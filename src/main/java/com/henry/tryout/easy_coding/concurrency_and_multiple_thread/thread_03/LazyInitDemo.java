777088fc3e413fe4b2de8136f3913b272c8f6d67
jenniferInSH <18770810604@163.com>
2022-08-04 22:54:09 +0800

    easy coding - volatile 共享变量对线程的可见性。

---
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_03/LazyInitDemo.java
@@ -0,0 +1,30 @@
+package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.thread_03;
+
+public class LazyInitDemo {
+    private static TransactionService service = null;
+
+    public static TransactionService getService() {
+        if (service == null) {
+            synchronized (LazyInitDemo.class) {
+                if (service == null) {
+                    // new对象本身不是一个原子操作
+                    /*
+                        new的三个步骤 - 这三个步骤可能会被重排
+                        #1 在内存中开辟一块区域；
+                        #2 调用初始化方法，完成对内存区域的初始化；
+                        #3 把内存区域的首地址 绑定到 变量上。
+
+                        问题的产生：线程A产生了一个没有被初始化的service，而线程B直接取用了这个service对象
+                     */
+                    service = new TransactionService();
+                }
+            }
+        }
+
+        return service;
+    }
+}
+
+class TransactionService {
+
+}
