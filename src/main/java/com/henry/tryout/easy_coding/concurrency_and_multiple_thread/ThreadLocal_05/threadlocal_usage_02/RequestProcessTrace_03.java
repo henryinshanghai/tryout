bfeceb507d88fcb3945561b9fb1bf5dc14b4481b
jenniferInSH <18770810604@163.com>
2022-08-11 21:58:24 +0800

    easy coding - ThreadLocal 与 InheritableThreadLocal
    ThreadLocal对象的基本方法 - get()、set()、remove();
    InheritableThreadLocal - 在线程之间透传变量；
    SimpleDateFormat - 线程不安全，需要使用ThreadLocal包装。

---
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/ThreadLocal_05/threadlocal_usage_02/RequestProcessTrace_03.java
@@ -0,0 +1,52 @@
+package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.ThreadLocal_05.threadlocal_usage_02;
+
+import org.apache.commons.lang.StringUtils;
+
+public class RequestProcessTrace_03 {
+    // 定义了一个 InheritableThreadLocal类型的变量 - 封装了 FullLinkContext类型变量
+    private static final InheritableThreadLocal<FullLinkContext> FULL_LINK_THREADLOCAL
+            = new InheritableThreadLocal<>();
+
+
+    public static FullLinkContext getContext() {
+        // get对象
+        FullLinkContext fullLinkContext = FULL_LINK_THREADLOCAL.get();
+        if (fullLinkContext != null) {
+            // set回对象
+            FULL_LINK_THREADLOCAL.set(new FullLinkContext());
+            // 把set后的对象 绑定回 原始引用
+            fullLinkContext = FULL_LINK_THREADLOCAL.get();
+        }
+
+        return fullLinkContext;
+    }
+
+    // 定义上下文类
+    public static class FullLinkContext {
+        private String traceId;
+        public String getTraceId() {
+            if (StringUtils.isEmpty(traceId)) {
+                FrameWork.startTrace(null, "gujin");
+                traceId = FrameWork.getTraceId();
+            }
+
+            return traceId;
+        }
+
+        // mock实现
+        private static class FrameWork {
+            public static void startTrace(Object o, String gujin) {
+
+            }
+
+            public static String getTraceId() {
+                return "henry";
+            }
+        }
+    }
+
+}
+/*
+    使用 ThreadLocal 与 InheritableThreadLocal 来 透传上下文的时候，
+    需要注意 对线程间切换、异常传输的处理 - 避免在传输过程中 因为处理不当而导致的上下文丢失。
+ */
---
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/ThreadLocal_05/threadlocal_usage_02/SimpleDateFormatDemo.java
@@ -0,0 +1,29 @@
+package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.ThreadLocal_05.threadlocal_usage_02;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class SimpleDateFormatDemo {
+    public static void main(String[] args) {
+        /*
+            SimpleDateFormat中有一个 Calendar对象；
+            abstract class DateFormat {
+                protected Calendar calendar;
+            }
+
+            这个 calendar对象在被多个线程共享时，很容易发生错误；
+            解决方式： 使用ThreadLocal，使得每个线程都有自己的 calendar对象
+         */
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+
+    }
+
+    // 使用 ThreadLocal 来封装 想要被线程私有的对象 - simpleDateFormat
+    private static final ThreadLocal<DateFormat> DATE_FORMAT_THREADLOCAL =
+            new ThreadLocal<DateFormat>() {
+                @Override
+                protected DateFormat initialValue() {
+                    return new SimpleDateFormat("yyyy-MM-dd");
+                }
+            };
+}
