37cac3bb6061500e0d36afa5e74c87561397c511
jenniferInSH <18770810604@163.com>
2022-08-21 23:09:43 +0800

    easy coding -
    recap & re-organized about 集合&泛型；
    
    泛型添加的约束 - List vs. List<?>
    List<泛型>之间的引用绑定 规则
    List <-> Array转换的手段 与 限制

--- src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/array_and_collection_04/ArrayUsageDemo_01.java
+++ src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/array_and_collection_04/ArrayUsageDemo_01.java
@@ -1,22 +1,20 @@
 package com.henry.tryout.easy_coding.data_structure_and_collection.array_and_collection_04;
 
-public class ArrayDemo_01 {
+// 数组的定义 & 初始化
+public class ArrayUsageDemo_01 {
     public static void main(String[] args) {
+        // 手段1：显式地初始化 {"xxx", "ooo"}
         String[] names1 = {"henry", "alicia"};
-        // 绑定到 Object类型的变量上
+        // 手段2：对Object对象做强制转化
         Object obj = names1;
-        // 从Object类型 强制转换成为 String[]
         ((String[]) obj)[0] = "object";
 
-        // 声明 + 静态初始化
-        String[] args3 = {"a", "b"};
-
-        // 声明 + 动态初始化
+        // 手段3：声明 + 动态初始化
         String[] args4 = new String[2];
         args4[0] = "a";
         args4[1] = "b";
 
-        // 遍历数组中的元素
+        // 操作：遍历数组中的元素
         for (String arg : args4) {
             System.out.println(arg);
         }
