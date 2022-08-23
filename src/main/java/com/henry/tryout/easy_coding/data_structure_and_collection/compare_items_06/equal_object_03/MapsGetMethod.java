2224c2d52ad47cf001e16b63ff54e0f1d81e5b0c
jenniferInSH <18770810604@163.com>
2022-08-23 22:45:16 +0800

    easy coding -
    recap & re-organized about 对象是否相等；
    
    map中get(key)时，如何判断key是否存在在散列表中；
    自定义类型的判等 - equals() + hashCode();
    list是否相同 - 取决于equals()方法的具体实现。
    
    Objects类的静态方法会很有用

--- src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/compare_items_06/equal_object_03/MapsGetMethod.java
+++ src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/compare_items_06/equal_object_03/MapsGetMethod.java
@@ -1,15 +1,15 @@
-package com.henry.tryout.easy_coding.data_structure_and_collection.compare_items_06;
+package com.henry.tryout.easy_coding.data_structure_and_collection.compare_items_06.equal_object_03;
 
 import java.util.HashMap;
 
-public class HashCode_and_equals_04 {
+public class MapsGetMethod {
     public static void main(String[] args) {
 
         HashMap<Object, Object> map = new HashMap<>();
 
         map.put("henry", "jane");
 
-        // get()方法中并没有预期的if判断
+        // get()方法中并没有预期的if判断 😳
         map.get("henry");
     }
 }
