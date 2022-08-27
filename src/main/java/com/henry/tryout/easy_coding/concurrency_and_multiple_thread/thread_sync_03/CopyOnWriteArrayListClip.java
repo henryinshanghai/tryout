b43753f93034621e8b2fbf9a268dd343c6b4e253
HenryInSH <2291972433@qq.com>
2022-08-27 23:10:13 +0800

    easy coding -
    recap & re-organized about volatile；
    
    volatile不是同步机制，无法处理 多线程并发写的场景(还是得加锁)；
    volatile只是解决了 多个线程之间变量的可见性问题；
    volatile的应用 - COWList;

--- src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/CopyOnWriteArrayListClip.java
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/CopyOnWriteArrayListClip.java
@@ -1,7 +1,9 @@
 package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.thread_sync_03;
 
+// volatile在 COWList中的应用
 public class CopyOnWriteArrayListClip {
     // 数组成员变量 - 用于真正地存储元素
+    // 使用 volatile修饰 - 这样某个线程对array的更新，对其他线程是立即可见的
     private transient volatile Object[] array;
 
     final void setArray(Object[] a) {
--- src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/VolatileNotAtomic_02.java
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/VolatileNotAtomic_02.java
@@ -1,17 +1,18 @@
 package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.thread_sync_03;
 
 public class VolatileNotAtomic_02 {
-    // volatile修饰的变量 - 保证多线程的可见性
+    // #1 volatile修饰的变量 - 作用：保证这个变量的更新 对多个线程都是立马可见的
     private static volatile long count = 0L;
     public static final int NUMBER = 10000;
 
     public static void main(String[] args) {
         Thread subtractThread = new SubtractThread();
-
+        // 触发 自定义线程run()方法的执行 - 手段：线程对象.start()
         subtractThread.start();
 
+        // 在主线程中，把count的值 + 10000
         for (int i = 0; i < NUMBER; i++) {
-            // 为 想要保证操作的原子性(否则的话，可能会出现多个线程对变量值的覆写)，需要为操作加锁
+            // #2 为了避免多个线程 对count写操作的相互覆盖，这里使用 synchronized关键字 来 保证线程之间操作count的互斥性
             synchronized (VolatileNotAtomic_02.class) {
                 count++;
             }
@@ -23,11 +24,13 @@ public class VolatileNotAtomic_02 {
         System.out.println("count变量最终的值为： " + count); // 这里count的值不是一个固定的结果
     }
 
-    // 静态内部类
+    // 自定义的线程类 - 手段： extends Thread
     private static class SubtractThread extends Thread {
 
+        // 在run()方法中，把count的值 - 10000
         public void run() {
             for (int i = 0; i < NUMBER; i++) {
+                // 定义临界区 - 手段：使用 synchronized关键字 来 修饰临界区
                 synchronized (VolatileNotAtomic_02.class) {
                     count--;
                 }
@@ -56,8 +59,8 @@ Java8中引入的支持 count++为原子操作的类型：
     AtomicLong, LongAdder(推荐使用)
 
 结论：
-    1 volatile并不是 轻量级的同步方式。而是 轻量级的 线程操作可见方式。
-    2 volatile变量在 多线程并发写的场景中，会出现线程安全问题。
+    1 volatile并不是 轻量级的"同步方式"。而是 轻量级的 "线程操作可见方式"。
+    2 volatile变量在 多线程并发写的场景中，会出现 线程安全问题。
     3 volatile变量 在一写多读的多线程场景中，不会有线程安全问题。
         应用： CopyOnWriteArrayList
             特征：在修改数据的时候，会：
@@ -67,6 +70,5 @@ Java8中引入的支持 count++为原子操作的类型：
             volatile的作用：
                 1 使读线程能够直接看到array的变化 - 因为写操作完成后，立刻变量的值立刻对读线程可见；
                 2 避免指令重排。
-
  */
 
