1745846c1183b1ac2392a49f83ee094e9b222121
HenryInSH <2291972433@qq.com>
2022-08-28 10:19:47 +0800

    easy coding -
    recap & re-organized about JDK中提供的用于支持多线程同步的类；
    
    1 CountDownLatch - 等待所有线程都执行完成后，主线程再继续执行；
    2 Semaphore - 线程执行时，抢占窗口。执行完成后，释放窗口（类比飞机场取票）；
    3 CyclicBarrier - 可以循环利用???

--- src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/semaphore_sync_03/CountDownLatchTest.java
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/semaphore_sync_03/CountDownLatchTest.java
@@ -3,9 +3,11 @@ package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.thread_sync
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+// 使用 countDownLatch 来 同步多个线程
+// 场景：主线程需要等待 其他多个线程执行结束，再继续执行 - 手段：countDownLatch
 public class CountDownLatchTest {
     public static void main(String[] args) throws InterruptedException {
-        // 准备一个 countDownLatch对象
+        // #1 准备一个 countDownLatch对象
         CountDownLatch count = new CountDownLatch(3);
 
         // 作为参数传入自定义线程的构造器中
@@ -13,20 +15,34 @@ public class CountDownLatchTest {
         Thread thread2 = new TranslateThread("2nd content", count);
         Thread thread3 = new TranslateThread("3rd content", count);
 
+        // note:先处理子线程异常的情况
+        thread1.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+            System.out.println(t.getName() + ": " + e.getMessage());
+        });
+        thread2.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+            System.out.println(t.getName() + ": " + e.getMessage());
+        });
+        thread3.setUncaughtExceptionHandler((Thread t, Throwable e) -> {
+            System.out.println(t.getName() + ": " + e.getMessage());
+        });
+
         // 依次启动三个线程 - 在线程的run()方法中，操作 CountDownLatch对象
         thread1.start();
         thread2.start();
         thread3.start();
 
-        // 调用 countDownLatch对象的await()方法 - 以等待所有的线程执行完成
+        // #4 调用 countDownLatch对象的await()方法 - 以等待 所有的线程（所有的, 就是使用 countDownLatch对象定义的） 执行完成
         count.await(10, TimeUnit.SECONDS);
+        System.out.println("所有线程执行完成");
         // 给调用方返回翻译的结果
     }
 }
 
+// 自定义线程类 - extends Thread
 class TranslateThread extends Thread {
     private String content;
-    private final CountDownLatch count; // 成员变量的初始化应该交给使用者 - 通过构造方法
+    // #2 子线程中 持有 倒计数门闩变量
+    private final CountDownLatch count; // 成员变量的初始化应该交给使用者 - via 构造方法
 
     public TranslateThread(String content, CountDownLatch count) {
         this.content = content;
@@ -37,7 +53,7 @@ class TranslateThread extends Thread {
     public void run() {
         // 在某些情况下，调用翻译引擎时，抛出了异常
         /*
-            这里抛出的异常没有被主线程 try-catch到 - 这会导致当前线程没能执行到 countDown()方法
+            这里抛出的异常 没有被主线程 try-catch到 - 这会导致 当前线程没能执行到 countDown()方法
             解决手段： 子线程中发生的异常 可以通过线程方法 setUncaughtExceptionHandler()方法来捕获
          */
         if (Math.random() > 0.5) {
@@ -45,6 +61,7 @@ class TranslateThread extends Thread {
         }
 
         System.out.println(content + "的翻译已经完成，译文是... ");
+        // #3 子线程中，调用countDown() - 每次运行run()的时候，count就-1. 当count=0时，latch will open
         count.countDown();
     }
 }
--- src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/semaphore_sync_03/CustomCheckWindow_02.java
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_sync_03/semaphore_sync_03/CustomCheckWindow_02.java
@@ -2,19 +2,22 @@ package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.thread_sync
 
 import java.util.concurrent.Semaphore;
 
+// 使用信号量 来 同步多个线程
 public class CustomCheckWindow_02 {
     public static void main(String[] args) {
-        // 设置3个信号量 - 3个检票窗口
+        // #1 设置大小为3的信号量 - 3个检票窗口
         Semaphore semaphore = new Semaphore(3);
 
-        // 这个队伍排了5个人
+        // #2 准备线程队伍 - 这个队伍排了5个人(线程)
         for (int i = 0; i <= 5; i++) {
             new SecurityCheckThread(i, semaphore).start();
         }
     }
 
+    // 自定义线程
     private static class SecurityCheckThread extends Thread {
         private int seq;
+        // 持有信号量类型的成员变量
         private Semaphore semaphore;
 
         public SecurityCheckThread(int seq, Semaphore semaphore) {
@@ -24,6 +27,7 @@ public class CustomCheckWindow_02 {
 
         public void run() {
             try {
+                // #3 获取到信号量中的一个信号(窗口)
                 semaphore.acquire();
                 System.out.println("No." + seq + "乘客，正在查验中...");
 
@@ -35,6 +39,7 @@ public class CustomCheckWindow_02 {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
+                // #4 释放掉信号量中的信号
                 semaphore.release();
                 System.out.println("No." + seq + "乘客已经完成查验。");
             }
