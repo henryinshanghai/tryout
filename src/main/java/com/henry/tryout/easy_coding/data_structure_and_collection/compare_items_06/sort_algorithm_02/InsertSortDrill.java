e09a7c77bf13a00ec1a82a06117242f13e580d37
HenryInSH <2291972433@qq.com>
2022-08-28 22:27:16 +0800

    基础排序 - 插入排序；
    技巧：定锚 cursorOfItemToInsert + 动指针 backwardCursor
    使用Comparable作为基础类型;
    使用 assert <boolean>作为时时调试手段。

--- src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/compare_items_06/sort_algorithm_02/InsertSortDrill.java
+++ src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/compare_items_06/sort_algorithm_02/InsertSortDrill.java
@@ -2,39 +2,60 @@ package com.henry.tryout.easy_coding.data_structure_and_collection.compare_items
 
 public class InsertSortDrill {
 
-    public Comparable[] sort(Comparable[] a) {
-        int length = a.length;
+    public static Comparable[] sort(Comparable[] a) {
+        int itemAmount = a.length;
 
-        for (int cursorOfItemToInsert = 1; cursorOfItemToInsert < length; cursorOfItemToInsert++) {
-            for (int backwardCursor = cursorOfItemToInsert; backwardCursor >=1 && less(a[backwardCursor], a[backwardCursor-1]); backwardCursor--) {
+        for (int anchorOfItemToInsert = 1; anchorOfItemToInsert < itemAmount; anchorOfItemToInsert++) {
+            for (int backwardCursor = anchorOfItemToInsert; backwardCursor >= 1 && less(a[backwardCursor], a[backwardCursor-1]); backwardCursor--) {
                 exch(a, backwardCursor, backwardCursor - 1);
             }
+
+            // 断言有序区间
+            assert isSorted(a, 0, anchorOfItemToInsert);
         }
 
+        assert isSorted(a);
         return a;
     }
 
-    private void exch(Comparable[] a, int i, int j) {
+    private static void exch(Comparable[] a, int i, int j) {
         Comparable temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
 
-    private boolean less(Comparable i, Comparable j) {
-        return i.compareTo(j) < 0;
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void printArr(Comparable[] a) {
+        for (Comparable item : a) {
+            System.out.print(item + " ");
+        }
+    }
+
+    // 断言 数组a已经排序
+    public static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+
+    // 断言 数组a的指定区间已经有序了
+    private static boolean isSorted(Comparable[] a, int leftBar, int rightBar) {
+        for (int cursor = leftBar; cursor <= rightBar; cursor++) {
+            if (less(a[cursor], a[cursor - 1])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
-        Integer[] a = {1, 5, 2, 0, 9, 4, 7, 1};
-        InsertSortDrill drill = new InsertSortDrill();
-        drill.sort(a);
+        Integer[] a = {2, 5, 1, 9, 3, 5, 7, 0};
+
+        sort(a);
+        assert isSorted(a);
 
         printArr(a);
-    }
-
-    private static void printArr(Comparable[] a) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i] + " ");
-        }
     }
 }
