dbb5d6edb289a9ead72d9c65be9a38b3b5990812
HenryInSH <2291972433@qq.com>
2022-09-11 17:53:29 +0800

    Java基础 - HashMap中get(certain_key)的实现
    检查传入的key是不是存在对应的Entry:
    1 hashCode(pass_in_key) == e.key;
    2 pass_in_key.equals(existing_key)方法。

--- src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/compare_items_06/equal_object_03/MapsGetMethod.java
+++ src/main/java/com/henry/tryout/easy_coding/data_structure_and_collection/compare_items_06/equal_object_03/MapsGetMethod.java
@@ -2,13 +2,16 @@ package com.henry.tryout.easy_coding.data_structure_and_collection.compare_items
 
 import java.util.HashMap;
 
+// 验证：hashmap中获取指定key对应的value时，会使用 hashCode()与equals()来验证 传入的key所对应的Entry
 public class MapsGetMethod {
     public static void main(String[] args) {
 
         HashMap<Object, Object> map = new HashMap<>();
 
+        // 向哈希结构中添加一个 henry-jane的元素
         map.put("henry", "jane");
 
+        // 从哈希结构中获取 key为henry的键值对 所对应的value的值
         /*
         getNode(hash(key), key)
 
@@ -18,7 +21,7 @@ public class MapsGetMethod {
 
             e.hash == hash // 先决条件，只有true时，才会执行后继的代码
 
-            哈希算法的追求：在equals()不相等时，hashCode也不想等 - 这样能够提升👆面代码的执行效率
+            哈希算法的追求：在equals()不相等时，hashCode也不相等 - 这样能够提升👆面代码的执行效率
                 1 使用自定义的对象作为map的key时，需要重写 hashCode() 与 equals()方法
                 2 使用自定义的对象 作为set的元素时，也需要重写 hashCode() 与 equals()方法 - 这两个方法会被用来对象的判等
          */
