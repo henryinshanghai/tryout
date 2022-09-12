08753d9a5d8bd3025060ebcc2d677b6cab7d922b
HenryInSH <2291972433@qq.com>
2022-09-12 17:32:31 +0800

    Java中的引用类型 - 弱引用
    如果一个对象被且仅被 一个弱引用所指向，则：在下一次YGC的时候，弱引用所指向的对象就会被回收掉；
    获取 弱引用所指向的对象 - 手段: 弱引用.get().

--- src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/ThreadLocal_05/reference_type_01/WeakReferenceWhenIdle_03.java
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/ThreadLocal_05/reference_type_01/WeakReferenceWhenIdle_03.java
@@ -2,14 +2,14 @@ package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.ThreadLocal
 
 import java.lang.ref.WeakReference;
 
-// 特性 - 如果某个对象 只被弱引用所指向，则：下一次YGC（新生代垃圾回收）时会回收此对象
+// 验证： 如果某个对象 只被弱引用所指向，则：下一次YGC（新生代垃圾回收）时会回收此对象
 public class WeakReferenceWhenIdle_03 {
     public static void main(String[] args) {
-        // seller强引用 指向 new House()对对象
+        // seller强引用 指向 house实例
         House seller = new House();
-        // buyer3引用 指向 弱引用对象; 弱引用对象 封装了 seller强引用指向的House对象
+        // buyer3引用 指向 弱引用对象;   而弱引用对象 封装了 seller强引用所指向的House对象
         WeakReference<House> buyer3 = new WeakReference<>(seller);
-        // 去除真实对象上的强引用
+        // 去除house对象上的强引用
         seller = null;
 
         long start = System.nanoTime();
@@ -31,7 +31,7 @@ public class WeakReferenceWhenIdle_03 {
     }
 }
 /*
-预期：希望在实际对象被置为null时，引用能够自动感知，并主动断开 引用所指向的对象。
+预期：希望在 实际对象被置为null 时，引用能够自动感知，并主动断开 引用所指向的对象。
 手段：弱引用 WeakReference。
 用法：使用 WeakReference就可以实现弱引用。
 预期：观察 GC触发的情况；
@@ -39,7 +39,7 @@ public class WeakReferenceWhenIdle_03 {
 结果：
     house is null and exited time = 32ms
 结论：
-    一段时间后，有且仅有 buyer3引用 指向的对象 new House()被回收掉了。 - 这时候使用 buyer3.get()得到的值为null
+    一段时间后，有且仅有 buyer3引用 指向的house实例对象被回收掉了。 -> 这时候使用 buyer3.get()得到的值为null
 
 弱引用的应用：
     WeakHashMap
