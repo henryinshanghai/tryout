ec7ebc6adef9d79623b18a9018d4d4d00413c96d
HenryInSH <2291972433@qq.com>
2022-09-12 16:34:01 +0800

    JDK中的线程池工具类 - 线程池参数 *拒绝策略 RejectHandler
    定义任务超过线程池处理能力时，怎样来拒绝任务

--- src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_pool_04/basic_usage_01/UserRejectHandler_04.java
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/thread_pool_04/basic_usage_01/UserRejectHandler_04.java
@@ -3,7 +3,8 @@ package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.thread_pool
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 
-// 自定义拒绝策略的类
+// 线程池构造方法的参数* - 自定义拒绝策略的类
+// 作用：指定当任务过多(工作线程 + 阻塞队列都处理不了)时，怎样来拒绝任务
 public class UserRejectHandler_04 implements RejectedExecutionHandler { // #1 implements RejectedExecutionHandler 拒绝的执行处理器
 
     @Override
