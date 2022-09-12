79c5066712c1d3acf90baf04cd9a451694b01a23
HenryInSH <2291972433@qq.com>
2022-09-12 17:23:58 +0800

    Java中的引用类型 - 软引用
    获取软引用指向的对象实例 - 软引用.get()
    特征：在JVM内存空间充足的时候，软引用指向的对象 不会被回收

--- src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/ThreadLocal_05/reference_type_01/SoftReferenceWhenIdle_02.java
+++ src/main/java/com/henry/tryout/easy_coding/concurrency_and_multiple_thread/ThreadLocal_05/reference_type_01/SoftReferenceWhenIdle_02.java
@@ -2,21 +2,23 @@ package com.henry.tryout.easy_coding.concurrency_and_multiple_thread.ThreadLocal
 
 import java.lang.ref.SoftReference;
 
+// 验证：当堆内存空间不紧张的时候，软引用指向的对象 不会被回收掉
 public class SoftReferenceWhenIdle_02 {
     public static void main(String[] args) {
         House seller = new House();
 
         // 第一处 - 只创建一个对象，并使用软引用指向此对象
         SoftReference<House> buyer2 = new SoftReference<>(seller); // 方案1
-        seller = null; // 把seller强引用指向别处
+        seller = null; // 把seller强引用指向别处, 这样 buyer2软引用 也就会指向到null
 
         while (true) {
-            // 建议 JVM进行垃圾回收 方案1
+            // 建议 JVM进行垃圾回收
             // 手段1：调用System.gc() - 具体时机仍然由JVM控制
             // 手段2：强制调用已经失去引用的对象的finalize()方法
             System.gc();
             System.runFinalization();
 
+            // 获取到软引用所指向的对象 - 手段: 软引用.get()
             if (buyer2.get() == null) {
                 System.out.println("house is null");
                 break;
