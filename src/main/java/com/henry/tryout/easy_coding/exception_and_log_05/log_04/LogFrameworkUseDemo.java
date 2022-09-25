b101119f9fdb098c7c4e2b8fce75ddf73ea8080c
HenryInSH <2291972433@qq.com>
2022-09-25 13:09:50 +0800

    日志
    在代码中使用日志门面：
    1 引入slf4j的静态类 LoggerFactory；
    2 调用LoggerFactory的静态方法，得到日志对象logger;
    3 调用logger的info()、debug()...等方法来打印日志

--- src/main/java/com/henry/tryout/easy_coding/exception_and_log_05/log_04/LogFrameworkUseDemo.java
+++ src/main/java/com/henry/tryout/easy_coding/exception_and_log_05/log_04/LogFrameworkUseDemo.java
@@ -1,13 +1,14 @@
 package com.henry.tryout.easy_coding.exception_and_log_05.log_04;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory; // #1 引入门面slf4j中的类
 
 public class LogFrameworkUseDemo {
     // 静态成员变量能够减少资源消耗 （因为与类绑定）
+    // #2 调用工厂类的静态方法 - 获取到日志对象
     private static final Logger logger = LoggerFactory.getLogger(LogFrameworkUseDemo.class);
 
     public static void main(String[] args) {
-
+        logger.info("invoking main method...");
     }
 }
--- src/main/java/com/henry/tryout/easy_coding/exception_and_log_05/log_04/printLog_01.java
+++ src/main/java/com/henry/tryout/easy_coding/exception_and_log_05/log_04/printLog_01.java
@@ -1,17 +1,24 @@
 package com.henry.tryout.easy_coding.exception_and_log_05.log_04;
 
-import java.util.logging.Logger;
+// #1 导入Logger对象
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+// 实践：在代码中使用日志的方式
 public class printLog_01 {
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger("com.henry.tryout.easy_coding.exception_and_log.log_04.printLog_01");
+        Logger logger = LoggerFactory.getLogger("com.henry.tryout.easy_coding.exception_and_log.log_04.printLog_01");
 
         // 条件输出方式 👇 这个logger对象 并没有 isDebugEnable()方法
 //        if (logger.isDebugEnable()) {
 //            logger.debug("xxx" + ooo + "xxx" + ooo);
 //        }
 
-        // 占位符方式 👇
-//        logger.debug("xxx {} xxx {}", oo, qq);
+        // 使用占位符方式 打印日志 👇
+        logger.info("invoking main method..."); // INFO
+        logger.debug("{} is bigger than {}", "10", "8"); // DEBUG
     }
 }
