62933685c205c7d81bd6a08a36e69118db156ee7
HenryInSH <2291972433@qq.com>
2022-10-08 20:03:26 +0800

    转换item的操作
    #1 使用map()可以完成单一item -> 单一item之间的转换操作;
    #2 如果中间操作产生了Stream<Stream<xxx>>这样的返回结果，则：使用flatMap()来把多个流转换成单一流

--- src/main/java/com/henry/tryout/java_8_features/stream_02/map_03/transformItemToElement_01.java
+++ src/main/java/com/henry/tryout/java_8_features/stream_02/map_03/transformItemToElement_01.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// 验证：使用stream能够把集合中的元素 转化成为新的元素；
+// 手段: list.stream().map(<transform_operation_via_lambda_expression>).xxx()
 public class transformItemToElement_01 {
     public static void main(String[] args) {
         transformDishToName();
@@ -23,7 +25,7 @@ public class transformItemToElement_01 {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
         List<Integer> squares = numbers.stream()
-                .map(number -> number * number)
+                .map(number -> number * number) // 使用lambda表达式 来表示 数值的转变过程: 乘方
                 .collect(Collectors.toList());
 
         squares.forEach(System.out::println);
@@ -33,7 +35,7 @@ public class transformItemToElement_01 {
         List<String> words = Arrays.asList("Java 8", "Lambdas", "In", "Action");
 
         words.stream()
-                .map(String::length)
+                .map(String::length) // 字符串的长度
                 .forEach(System.out::println);
     }
 
@@ -42,11 +44,11 @@ public class transformItemToElement_01 {
 
         // 把 item 转化成为 element
         /*
-            转化的手段1： 方法引用 - 什么时候可以使用方法引用 来 简化lambda表达式来着？
-                当lambda表达式做的事情只是 - 把参数转发给实际被执行的方法 时.
+            lambda表达式的语法糖： 方法引用；
+                如果 lambda表达式做的事情只是“把参数转发给实际被执行的方法”，则：可以使用方法引用的语法。
          */
         List<String> dishNames = menu.stream()
-                .map(Dish::getName)
+                .map(Dish::getName) // dish对象的属性
                 .collect(Collectors.toList());
 
         dishNames.forEach(System.out::println);
--- src/main/java/com/henry/tryout/java_8_features/stream_02/map_03/transformItemToStream_02.java
+++ src/main/java/com/henry/tryout/java_8_features/stream_02/map_03/transformItemToStream_02.java
@@ -5,20 +5,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+// 验证：如果中间操作得到了 多个流对象Stream<Stream<xxx>>，则：可以使用 flatMap()方法，连接多个流成单一流
+// 手段：得到Stream<Stream<xxx>>的中间操作 - {字符串被拆分成字符数组, 组合两个list中的元素}
+// tip：IDEA会对当前操作得到的结果类型做出提示，如果是Stream<Stream<xxx>>，则：可以尝试使用下flatMap
 public class transformItemToStream_02 {
     public static void main(String[] args) {
 
-        // 获取到 单词列表中 所有使用到的字母集合
+        // 获取到 单词列表中的每个单词 所使用到的所有字母的集合
         printUniqueCharacterSet();
 
         System.out.println("=============");
 
-        // 创建所有的数对组合
+        // 构建所有的数对组合
         printAllNumberPair();
 
         System.out.println("*************");
 
-        // 返回 加和值可以被3整除的数对组合
+        // 返回 加和值可以被3整除 的数对组合
         printPairsDivisibleByThree();
 
     }
@@ -65,20 +68,23 @@ public class transformItemToStream_02 {
     private static List<String> getUniqueCharacterSet() {
         List<String> words = Arrays.asList("Hello", "World");
 
-        // 获取到所有单词所使用的字母的集合（去重）
-        // 使用map()得不到想要的List<String>结果
+        // 目标：获取到所有单词 所使用的所有字母 的集合（去重）
+        // 特征：使用map()得不到想要的List<String>结果
+        // 验证如下 👇
         List<Stream<String>> collect = words.stream()
                 .map(word -> word.split("")) // item: String -> String[]
                 .map(Arrays::stream) // item: String[] -> Stream<String>
                 .distinct()
                 .collect(Collectors.toList());
 
-        // 把所有的流连接起来，得到单一的流
+        // 难点：需要把所有的流连接起来，得到单一的流
         // 手段： flatMap(Stream<Stream<String>>)
+        // 验证如下 👇
         List<String> uniqueCharacters = words.stream()
                 .map(word -> word.split(""))
-                .flatMap(Arrays::stream)
+                .flatMap(Arrays::stream) // 从多个Stream得到单一的Stream - 手段：flatMap()
                 .collect(Collectors.toList());
+
         return uniqueCharacters;
     }
 }
