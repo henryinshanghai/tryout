2937d814dcfa559ee51077c0c7d201b1c84ebdca
HenryInSH <2291972433@qq.com>
2022-10-09 19:09:24 +0800

    数值流
    #1 JDK中提供了工具类来快速创建出数值流，而不需要从某个数据源来衍生出数值流 - IntStream;
    #2 使用工具类的静态方法能够创建出需要的数值流 - IntStream.rangeClosed(leftBar, rightBar);

--- src/main/java/com/henry/tryout/java_8_features/stream_02/data_stream_07/data_range_demo_02.java
+++ src/main/java/com/henry/tryout/java_8_features/stream_02/data_stream_07/data_range_demo_02.java
@@ -2,6 +2,8 @@ package com.henry.tryout.java_8_features.stream_02.data_stream_07;
 
 import java.util.stream.IntStream;
 
+// 验证：JDK为数值流提供了多种工具类，用于快速创建出需要的数值流。
+// 创建指定范围内的整数所构成的数值流 - IntStream.rangeClosed(leftBar, rightBar)
 public class data_range_demo_02 {
     public static void main(String[] args) {
         // 生成 [1, 100]数值区间的所有数字
