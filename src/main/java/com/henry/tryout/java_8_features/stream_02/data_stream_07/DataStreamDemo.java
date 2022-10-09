904b92d0b44daecc486ce7c6bb53f36463cccbc4
HenryInSH <2291972433@qq.com>
2022-10-09 18:53:41 +0800

    数值流
    #1 数值流提供了一些数据处理的快捷API - max()
    #2 从一般流得到数值流 - mapToInt(lambda_return_int)
    #3 从数值流得到一般流 - data_stream.boxed()

--- src/main/java/com/henry/tryout/java_8_features/stream_02/data_stream_07/DataStreamDemo.java
+++ src/main/java/com/henry/tryout/java_8_features/stream_02/data_stream_07/DataStreamDemo.java
@@ -8,6 +8,10 @@ import java.util.OptionalInt;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+// 验证：对数值的流式处理，JDK中提供了快捷方式 - 数值流 & 数值流上的快捷api
+// 把一般流转化成为数值流 - 手段：mapToInt(lambda_return_int)
+// 把数值流转换回一般流 - 手段：intStream.boxed()
+// 获取数值流中的最大值 - 手段： xxx.max();
 public class DataStreamDemo {
     public static void main(String[] args) {
         List<Dish> menu = new Menu().getMenu();
@@ -17,8 +21,6 @@ public class DataStreamDemo {
 
         // 把 数值流 转化回去成 一般流 - boxed()方法
         transformStream(menu);
-
-        System.out.println("======================");
 
         // 找到热量最高的菜品，并获取到它的卡路里
         printMaxDishCalories(menu);
