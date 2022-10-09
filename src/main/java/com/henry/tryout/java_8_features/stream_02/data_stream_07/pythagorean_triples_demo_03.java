ef303422003fcdefb1798ef8a4ba8bb7034be9ac
HenryInSH <2291972433@qq.com>
2022-10-09 19:20:04 +0800

    数值流
    #1 使用JDK中提供了数值流工具类能够直接创建出 勾股数三元组
    #2 数值流 转化成为 一般流：data_stream.boxed()
    #3 求取勾股数 - Math.sqrt(a * a + b * b) % 1 == 0
    #4 从既有元素衍生出一个新的元素 - xxx.mapToObj(create_new_obj)

--- src/main/java/com/henry/tryout/java_8_features/stream_02/data_stream_07/pythagorean_triples_demo_03.java
+++ src/main/java/com/henry/tryout/java_8_features/stream_02/data_stream_07/pythagorean_triples_demo_03.java
@@ -3,6 +3,9 @@ package com.henry.tryout.java_8_features.stream_02.data_stream_07;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+// 验证：使用JDK提供的工具类 能够创建出 勾股数三元组
+// 手段：对于任意整数a，找到整数b - 使整数b满足 a^2 + b^2 的取根号结果为整数 - 取余1等于0
+// 验证：能够从数值流上的元素映射出一个新的元素 - 手段：xxx.mapToObj(<create_new_item_base_exist_item>)
 public class pythagorean_triples_demo_03 {
     public static void main(String[] args) {
         // 生成 毕达哥拉斯三元组
