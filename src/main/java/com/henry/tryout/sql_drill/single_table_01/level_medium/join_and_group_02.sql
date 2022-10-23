b18be6092b62c728f298ff847eb88123bc611f0a
HenryInSH <2291972433@qq.com>
2022-10-23 12:24:09 +0800

    join其他表的作用 {查询所需要的其他表中的字段, 确定当前表结果集的legit scope}； group by的作用 - 产生出需要的子组，来统计需要的聚合结果

--- src/main/java/com/henry/tryout/sql_drill/single_table_01/level_medium/join_and_group_02.sql
+++ src/main/java/com/henry/tryout/sql_drill/single_table_01/level_medium/join_and_group_02.sql
@@ -1,9 +1,10 @@
--- 每个学校(#1 ) 答过题的用户(#2) 平均答题数量情况(#3)
+-- 每个学校(#1) 答过题的用户(#2) 平均答题数量情况(#3)
 select
-  university,
-  count(question_id) / count(distinct(answer.device_id)) as avg_answer_cnt -- 平均答题数量情况(#3)
+  user.university,
+  count(question_id) / count(distinct(answer.device_id)) as avg_answer_cnt -- #3 平均答题数量情况 = 答题总数量 / 答题总人数
 from
-  question_practice_detail answer -- 答过题的用户(#2)
-join user_profile user on answer.device_id = user.device_id
+  question_practice_detail answer -- 答题明细表
+join user_profile user -- #2 join用户表 来 查询到university字段信息
+on answer.device_id = user.device_id
 group by
-  university -- 每个学校(#1)
+  user.university -- #1 按照university分组 来 查询每个学校
