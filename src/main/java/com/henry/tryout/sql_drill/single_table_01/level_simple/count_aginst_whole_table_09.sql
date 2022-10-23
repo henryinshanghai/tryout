598ceeb68639fad2f2c04ad47a96c8cd1b6400c8
HenryInSH <2291972433@qq.com>
2022-10-23 11:57:57 +0800

    可以对整张表(不需要分组)直接做统计，统计时根据具体业务需要可以使用distinct去重

--- src/main/java/com/henry/tryout/sql_drill/single_table_01/level_simple/count_aginst_whole_table_09.sql
+++ src/main/java/com/henry/tryout/sql_drill/single_table_01/level_simple/count_aginst_whole_table_09.sql
@@ -1,10 +1,10 @@
---  21年8月份 练题总数
+--  2021年8月份(#1) 所有练习过题目的总用户数(#2) 和 练习过题目的总次数(#3)
 select
-  count(distinct device_id) as did_cnt, -- 一人可能做多题 所以使用distinct关键字
-  count(question_id) as question_cnt
+  count(distinct device_id) as did_cnt, -- #2 统计人数时，由于一人可能做多题 所以使用distinct关键字来去重
+  count(question_id) as question_cnt -- #3
 from
   question_practice_detail
 where
   year(date) = 2021
-  and month(date) = 8 -- 没有group by时，也可以使用 count()对整个组的记录进行计数
+  and month(date) = 8 -- #1 没有group by时，也可以使用 count()对整个组的记录进行计数
 
