d5ca75df25023222b3f018256f44ce00f298a5f6
HenryInSH <2291972433@qq.com>
2022-10-23 17:41:28 +0800

    查询出自定义的新字段,并新字段手动绑定值 - 手段:case...when / if(condition, value1, value2)

--- src/main/java/com/henry/tryout/sql_drill/single_table_01/level_medium/select_new_field_via_case_when_07.sql
+++ src/main/java/com/henry/tryout/sql_drill/single_table_01/level_medium/select_new_field_via_case_when_07.sql
@@ -8,7 +8,7 @@ select
     when age < 25 and age >= 20 then '20-24岁'
     when age >= 25 then '25岁及以上'
     else '其他'
-  end as age_cut -- #1 根据条件 select出自定义的新字段 手段：case...when
+  end as age_cut -- #1 根据条件 select出自定义的新字段 手段：case...when 特征：新字段的值会根据case...when的执行情况确定
 from
   user_profile -- 用户的明细情况(#3)
 -- #2 这里题目没有要求进行分组后统计，因此不用group by
--- src/main/java/com/henry/tryout/sql_drill/single_table_01/level_medium/select_new_field_via_if_06.sql
+++ src/main/java/com/henry/tryout/sql_drill/single_table_01/level_medium/select_new_field_via_if_06.sql
@@ -1,7 +1,7 @@
 -- 计算25岁以上 和 以下(#1) 的用户数量(#2)
 -- if（x=n,a,b）表示如果x=n,则返回a，否则就是b了
 select
-    if(age>=25,'25岁及以上','25岁以下') as age_cut, -- #1 select 新字段age_cut '25岁及以上','25岁以下'会成为字段的值
+    if(age>=25,'25岁及以上','25岁以下') as age_cut, -- #1 select out 新字段age_cut - '25岁及以上','25岁以下'会成为字段的值
     count(device_id) as number -- #2 最后对子组进行统计
 from user_profile
 group by age_cut -- 再根据新的字段 age_cut进行分组
