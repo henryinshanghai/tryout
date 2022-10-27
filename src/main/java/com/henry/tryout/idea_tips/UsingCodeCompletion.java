9c9c3ddbd84bb3dc2485f15642f4c8500a9fe242
HenryInSH <2291972433@qq.com>
2022-10-27 09:07:08 +0800

    --- idea tip: 键入关键字母，使用代码补全的方式来生成样板代码 ---

---
+++ src/main/java/com/henry/tryout/idea_tips/UsingCodeCompletion.java
@@ -0,0 +1,10 @@
+package com.henry.tryout.idea_tips;
+
+public class UsingCodeCompletion {
+    private String name;
+
+    // 键入gn, 就能够使用代码补全的方式 来 生成样板代码getName()
+    public String getName() {
+        return name;
+    }
+}
