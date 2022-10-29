ba00b948f86f36914bbbf74ee99a06a99629765b
HenryInSH <2291972433@qq.com>
2022-10-29 18:19:28 +0800

    --- idea tip: 把Java语法编写的类 转化成同等语义的Kotlin文件（需要引入对Kotlin的依赖 & 用法不纯熟） ---

---
+++ src/main/java/com/henry/tryout/idea_tips/today/ConvertJavaClass.kt
@@ -0,0 +1,13 @@
+package com.henry.tryout.idea_tips.today
+
+// 这是从Java类 转化得到的一个 Kotlin类型 - 更加简洁了
+class ConvertJavaClass {
+    var age = 0
+    var name: String? = null
+
+    constructor() {}
+    constructor(age: Int, name: String?) {
+        this.age = age
+        this.name = name
+    }
+}
