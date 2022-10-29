bd87eaa0287b699b875c9fe2289510cfcbbce67b
HenryInSH <2291972433@qq.com>
2022-10-29 12:32:16 +0800

    --- idea tip: 运行测试类中的所有测试方法 ---

---
+++ src/main/java/com/henry/tryout/idea_tips/RunMethods.java
@@ -0,0 +1,21 @@
+package com.henry.tryout.idea_tips;
+
+import org.junit.jupiter.api.Test;
+
+public class RunMethods {
+
+    @Test
+    public void testMethod01() {
+        System.out.println("execute testMethod01");
+    }
+
+    @Test
+    public void testMethod02() {
+        System.out.println("execute testMethod02");
+    }
+
+    @Test
+    public void testMethod03() {
+        System.out.println("execute testMethod03");
+    }
+}
