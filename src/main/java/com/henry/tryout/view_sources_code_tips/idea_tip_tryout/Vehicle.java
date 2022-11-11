8d2ef5cffaa4d2952878798917c7cb8eef69aa87
HenryInSH <2291972433@qq.com>
2022-11-11 22:27:35 +0800

    --- 查看源码的技巧: 工具窗口查看方法的usage Alt + F7 ---

--- src/main/java/com/henry/tryout/view_sources_code_tips/idea_tip_tryout/Vehicle.java
+++ src/main/java/com/henry/tryout/view_sources_code_tips/idea_tip_tryout/Vehicle.java
@@ -7,5 +7,6 @@ public interface Vehicle {
     // 从弹窗形式 切换到 工具窗口形式 - 右键 右上角小窗口
     // 退出工具窗口 - Shift + Esc
     // 查看方法的hierarchy - Shift + Ctrl + H
+    // 以工具窗口方式查看 方法的usage - Alt + F7
     void move();
 }
