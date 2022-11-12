e547b48c76b5e396f53f148ac0da99b520cc5c9d
HenryInSH <2291972433@qq.com>
2022-11-12 10:41:24 +0800

    ### 软件设计之可测试性 - 反例 {单例模式, 成员变量&初始化} ###

---
+++ src/main/java/com/henry/tryout/software_design/testability/MySingleton.java
@@ -0,0 +1,11 @@
+package com.henry.tryout.software_design.testability;
+
+public class MySingleton {
+    private String name;
+
+    private MySingleton() {
+        name = "Henry";
+    }
+
+
+}
---
+++ src/main/java/com/henry/tryout/software_design/testability/TryToExtendFromSingleton.java
@@ -0,0 +1,6 @@
+package com.henry.tryout.software_design.testability;
+
+// 验证：对于构造器是私有的类，没办法使用一个子类继承它
+//public class TryToExtendFromSingleton extends MySingleton{
+//
+//}
