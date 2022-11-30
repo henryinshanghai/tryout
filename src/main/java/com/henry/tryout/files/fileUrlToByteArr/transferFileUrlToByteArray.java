659f069bcd0b02d83db25c4fe59101ab51a93bf9
HenryInSH <2291972433@qq.com>
2022-11-30 22:17:03 +0800

    ### 从文件URL中读取得到byte[]的步骤 - {#1 初始化URL对象; #2 开启流; #3 IOUtils把流转换成为ByteArray} ###

--- src/main/java/com/henry/tryout/files/fileUrlToByteArr/transferFileUrlToByteArray.java
+++ src/main/java/com/henry/tryout/files/fileUrlToByteArr/transferFileUrlToByteArray.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+// 工具：从远端文件URL中读取出二进制数组
 public class transferFileUrlToByteArray {
     public static void main(String[] args) {
         String fileUrl = "https://github.com/henryinshanghai/little_prince/blob/master/chapter01.txt";
@@ -16,15 +17,19 @@ public class transferFileUrlToByteArray {
             e.printStackTrace();
         }
 
+        // 这里的byteArr打印出来是一个 SHA01
         System.out.println("this is us" + byteArr);
     }
 
     private static byte[] fetchRemoteFile(String fileUrl) throws IOException {
+        // #1 使用url初始化得到 URL对象
         URL url = new URL(fileUrl);
         InputStream is = null;
         byte[] bytes = null;
         try {
-            is = url.openStream(); // 为什么今天下午这里会报错呢？
+            // #2 从URL对象中，打开流
+            is = url.openStream();
+            // #3 使用IOUtils 的静态方法 toByteArray() 来 把流转化成为byte[]
             bytes = IOUtils.toByteArray(is);
         } catch (IOException e) {
             //handle errors
