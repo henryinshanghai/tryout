4f4831afd7b0296c9abc6560e0a144815f442ad7
HenryInSH <2291972433@qq.com>
2022-12-11 20:18:13 +0800

    ### Spring Security模型的分类 - 核心模型 {Authentication, AuthenticationManager}、扩展模型、入口类组件  ###

---
+++ src/main/java/com/henry/tryout/spring_security_demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.henry.tryout.spring_security_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
