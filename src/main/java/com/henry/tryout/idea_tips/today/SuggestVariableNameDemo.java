368462770aeffbde19079a9a4f4595c5e7af9bf5
HenryInSH <2291972433@qq.com>
2023-01-15 15:56:47 +0800

    --- 验证 Ctrl+空格的快捷键 能不能建议出一个变量名 - 结果：失败 ---

---
+++ src/main/java/com/henry/tryout/idea_tips/today/SuggestVariableNameDemo.java
@@ -0,0 +1,8 @@
+package com.henry.tryout.idea_tips.today;
+
+import java.io.FileOutputStream;
+
+// 验证：使用 Ctrl+空格 能够得到 IDEA所建议的字段名 - 结果：失败
+public class SuggestVariableNameDemo {
+    private FileOutputStream fileOutputStream;
+}
