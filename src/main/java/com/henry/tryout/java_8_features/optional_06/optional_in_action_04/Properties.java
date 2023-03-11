1593fba9c917be3e942735f535c9d3ab486c7816
HenryInSH <2291972433@qq.com>
2023-03-12 00:04:55 +0800

    --- 常见实践 - 在类的init()方法中，对类中的成员变量进行初始化（绑定值的操作） ---

--- src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/Properties.java
+++ src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/Properties.java
@@ -3,6 +3,7 @@ package com.henry.tryout.java_8_features.optional_06.optional_in_action_04;
 import java.util.HashMap;
 import java.util.Map;
 
+// 验证：类的init()方法/初始化方法/构造方法中，应该要完成 对成员变量的初始化
 public class Properties {
 
     private Map<String, String> hostMap = new HashMap<>();
