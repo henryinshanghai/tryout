4c5ce55f091f723795308c37d64a9a7887359b0d
HenryInSH <2291972433@qq.com>
2023-03-12 00:10:33 +0800

    --- Optional中的4个方法 {#1 ofNullable() 获得optional对象; #2 flatMap() 把参数扁平化成为一个简单的optional对象; #3 filter() 对optional对象中封装的值进行筛选; #4 orElse() 用于设置值为null时返回的默认值} ---

--- src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/using_optional_to_avoid_trivial_grammar_03.java
+++ src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/using_optional_to_avoid_trivial_grammar_03.java
@@ -2,6 +2,10 @@ package com.henry.tryout.java_8_features.optional_06.optional_in_action_04;
 
 import java.util.Optional;
 
+// 验证：ofNullable() 用于接收一个可能为null的参数，返回一个 optional对象；
+// 验证：flatMap() 用于把参数（不管是原始对象 还是 optional对象），都扁平化成为一个 optional对象；
+// 验证：filter() 用于过滤optional对象所封装的value；
+// 验证： orElse() 用于在取值为null时，提供一个默认的值；
 public class using_optional_to_avoid_trivial_grammar_03 {
     public static void main(String[] args) {
         /*
@@ -9,14 +13,11 @@ public class using_optional_to_avoid_trivial_grammar_03 {
                 1 一堆的属性 -> 值的映射；
                 2 client代码需要读取这些属性，如果属性的值大于0，返回属性值。如果是其他情况，则返回0
          */
-
         Properties properties = new Properties();
         properties.init();
 
         int result = readDurationNotUsingOptional(properties, "atrrA");
-
         int result_optional = readDurationUsingOptional(properties, "atrrA");
-
     }
 
     private static int readDurationUsingOptional(Properties properties, String name) {
