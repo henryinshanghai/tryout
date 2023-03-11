43a61ddbfa9fa0fbc94b3f693dd12ac80c809e96
HenryInSH <2291972433@qq.com>
2023-03-11 23:36:34 +0800

    --- Optional用法：使用optional对象的 filter() + ifPresent()方法，能够避免传值为null时发生NPE ---

--- src/main/java/com/henry/tryout/java_8_features/optional_06/optional_usage_pattern_03/Optional_Usage_Pattern_03.java
+++ src/main/java/com/henry/tryout/java_8_features/optional_06/optional_usage_pattern_03/Optional_Usage_Pattern_03.java
@@ -4,6 +4,8 @@ import com.henry.tryout.java_8_features.optional_06.optional_in_java_02.Insuranc
 
 import java.util.Optional;
 
+// 验证：当使用if条件做属性值的筛选时，需要先判断不为null，然后才能取值 - 否则就会有NPE的风险
+// 验证： 使用optional对象的 filter()方法 + ifPresent()方法 - 这种方式，不会出现NPE。因为参数为null时，调用不会发生
 public class Optional_Usage_Pattern_03 {
     public static void main(String[] args) {
         // 使用filter筛选掉某些特定的值
@@ -18,7 +20,7 @@ public class Optional_Usage_Pattern_03 {
         // 使用optional对象的filter()来做筛选
         /*
             特征：
-                1 filter()方法中使用的对象 就是一个 已经解引用的对象 - 不用担心会导致NPE（引入如果对象为null，调用根本就不会发生）
+                1 filter()方法中使用的对象 就是一个 已经解引用的对象 - 不用担心会导致NPE（因为如果对象为null，调用根本就不会发生）
                 2 如果filter的结果为true，则：filter()方法会直接返回 Optional对象。否则，把当前值过滤掉，把optional对象的值置空
          */
         optInsurance.filter(insurance ->
