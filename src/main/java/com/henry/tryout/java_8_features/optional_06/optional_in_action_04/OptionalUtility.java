948c7b22e07ee6343f725e2d5440cefc9444e869
HenryInSH <2291972433@qq.com>
2023-03-12 00:08:34 +0800

    --- JDK中“表示值不存在”的策略#2 - 抛出一个异常。 实践：为了避免语法代码喧宾夺主，建议开发者把 原始语句 + 语法语句 封装到一个静态方法中。 ---

--- src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/OptionalUtility.java
+++ src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/OptionalUtility.java
@@ -2,6 +2,7 @@ package com.henry.tryout.java_8_features.optional_06.optional_in_action_04;
 
 import java.util.Optional;
 
+// 验证：好的实践 - 当使用Optional 来 封装可能抛出异常的语句时，把 原始语句 + 处理异常的语句 封装到一个新的静态方法中。
 public class OptionalUtility {
 
     // 把原始API抛出异常的场景，转化成为一个 optional的空对象 - 好处：client代码不再需要 笨拙的 try/catch语法了
--- src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/using_optional_to_capsule_throw_exception_02.java
+++ src/main/java/com/henry/tryout/java_8_features/optional_06/optional_in_action_04/using_optional_to_capsule_throw_exception_02.java
@@ -2,6 +2,8 @@ package com.henry.tryout.java_8_features.optional_06.optional_in_action_04;
 
 import java.util.Optional;
 
+// 验证： JDK中 用于表示“值不存在”的另一个策略 - 抛出一个异常。 这会强制要求开发者编写 try/catch的代码
+// 手段：把原始代码 + try/catch的语法代码，封装到一个 静态方法中去
 public class using_optional_to_capsule_throw_exception_02 {
     public static void main(String[] args) {
         // 除了返回一个null,另外一个常见的 表示值不存在的方式是 - 抛出一个异常
@@ -14,7 +16,4 @@ public class using_optional_to_capsule_throw_exception_02 {
         System.out.println("henryString 的数值为： " + henryString);
 
     }
-
-
-
 }
