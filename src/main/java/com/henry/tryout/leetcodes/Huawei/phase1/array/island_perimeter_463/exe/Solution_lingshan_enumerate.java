c28f1b56961fd7262f23cdcfe51c4fc64c72153c
HenryInSH <2291972433@qq.com>
2026-01-25 20:43:04 +0800

    --- leetcode463 岛屿的周长（暴露在外沿的边）：① 遍历二维数组中的所有方格，找到 '当前的陆地方格'；② 从 陆地方格 向 上下左右四个方向 扩展；③ 如果从 当前的陆地方格 走向了 非陆地方格（出界 或 水方格），则：当前方向 为 岛屿的边长 贡献1；④ 也可以使用DFS实现，但不推荐（因为题目保证了只有一个岛） ---

---
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase1/array/island_perimeter_463/exe/Solution_lingshan_enumerate.java
@@ -0,0 +1,38 @@
+package com.henry.tryout.leetcodes.Huawei.phase1.array.island_perimeter_463.exe;
+
+public class Solution_lingshan_enumerate {
+    public int islandPerimeter(int[][] grid) {
+        int rowAmount = grid.length;
+        int colAmount = grid[0].length;
+        int islandPerimeter = 0;
+
+        for (int currentRow = 0; currentRow < rowAmount; currentRow++) {
+            for (int currentCol = 0; currentCol < colAmount; currentCol++) {
+                if (grid[currentRow][currentCol] == 0) {
+                    continue;
+                }
+
+                // 上：出界或者是水
+                if (currentRow == 0 || grid[currentRow - 1][currentCol] == 0) {
+                    islandPerimeter++;
+                }
+
+                // 下：出界或者是水
+                if (currentRow == rowAmount - 1 || grid[currentRow + 1][currentCol] == 0) {
+                    islandPerimeter++;
+                }
+
+                // 左：出界或者是水
+                if (currentCol == 0 || grid[currentRow][currentCol - 1] == 0) {
+                    islandPerimeter++;
+                }
+
+                // 右：出界或者是水
+                if (currentCol == colAmount - 1 || grid[currentRow][currentCol + 1] == 0) {
+                    islandPerimeter++;
+                }
+            }
+        }
+        return islandPerimeter;
+    }
+}
---
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase1/array/island_perimeter_463/exe/Solution_qianwen_DFS_on_island.java
@@ -0,0 +1,55 @@
+package com.henry.tryout.leetcodes.Huawei.phase1.array.island_perimeter_463.exe;
+
+public class Solution_qianwen_DFS_on_island {
+    public int islandPerimeter(int[][] grids) {
+        int rowAmount = grids.length;
+        int colAmount = grids[0].length;
+
+        for (int currentRow = 0; currentRow < rowAmount; currentRow++) {
+            for (int currentCol = 0; currentCol < colAmount; currentCol++) {
+                // 如果 当前方格的值为1，说明 是陆地方格，则：
+                if (grids[currentRow][currentCol] == 1) {
+                    // 以它作为起点，在矩阵中 开始DFS - 标记所有 与之相连通的方格（也就是 岛屿中的所有方格）
+                    return dfs(grids, currentRow, currentCol);
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * 🌟 核心思想：
+     * 周长 = 所有陆地格子“面向外部的边”的总和
+     * DFS 通过“遇到外部就返回 1”来精确统计这些边。
+     *
+     * @param grids      方格的二维数组
+     * @param currentRow 当前方格的行坐标
+     * @param currentCol 当前方格的列坐标
+     * @return 以当前方格作为起点的 岛屿区域的周长贡献（暴露在外的边的数量）
+     */
+    private int dfs(int[][] grids, int currentRow, int currentCol) {
+        /* 每当在 DFS 遍历中，从 一个岛屿方格 走向 一个非岛屿方格，就 将周长加 1 */
+        // 如果 当前方向的方格 是非岛屿方格（越界 或 遇到水），说明 它会为周长 贡献一条边，则：
+        if (currentRow < 0 || currentRow >= grids.length // 行坐标 越界
+                || currentCol < 0 || currentCol >= grids[0].length // 列坐标 越界
+                || grids[currentRow][currentCol] == 0) { // 当前方格 是 水方格
+            // 为 上一级调用 返回1(周长+1)；
+            return 1;
+        }
+
+        // 如果 当前方格的值 是-1，说明 它是一个 已经访问过的方格，不会再 为周长 贡献边 了，则：
+        if (grids[currentRow][currentCol] == -1) {
+            // 为 上一级调用 返回0；
+            return 0;
+        }
+
+        // 搜索到 当前方格，把 当前方格 标记为 已访问
+        grids[currentRow][currentCol] = -1;
+
+        // 递归地搜索 当前方格的四个方向（上下左右），并 把每个方向上 得到的边数 累加起来
+        return dfs(grids, currentRow - 1, currentCol) +
+                dfs(grids, currentRow + 1, currentCol) +
+                dfs(grids, currentRow, currentCol - 1) +
+                dfs(grids, currentRow, currentCol + 1);
+    }
+}
