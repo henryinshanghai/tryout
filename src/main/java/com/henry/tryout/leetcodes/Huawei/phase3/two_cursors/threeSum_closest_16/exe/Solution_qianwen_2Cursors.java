4f3a064dde81a07a9a7835a1e590c2c55e31b9fb
HenryInSH <2291972433@qq.com>
2026-01-30 22:54:38 +0800

    --- leetcode16 从整数序列中找到3个数字，使得它们的加和最接近targetSum（排序、定指针+动指针）：① 对整数序列进行排序（Arrays.sort()）；② 从当前位置开始，尝试找到所有能够满足题意的三元组（while循环 + middleBro指针、bigBro指针）；③ ‘最接近的加和值’closestSum变量（初始化为 nums[0] + nums[1] + nums[2]）；④ 尝试更新‘最接近的加和值’（Math.abs(sumOfCurrentCombo - targetSum)）；⑤ 根据与targetSum的比较结果，按需移动指针（大则左移，小则右移）。 ---

---
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/two_cursors/threeSum_closest_16/exe/Solution_qianwen_2Cursors.java
@@ -0,0 +1,49 @@
+package com.henry.tryout.leetcodes.Huawei.phase3.two_cursors.threeSum_closest_16.exe;
+
+import java.util.Arrays;
+
+public class Solution_qianwen_2Cursors {
+    public int threeSumClosest(int[] numArr, int targetSum) {
+        // 对数组元素进行排序，得到 有序的数字序列
+        Arrays.sort(numArr);
+
+        int numAmount = numArr.length;
+        // 准备一个 ‘最接近target的sum’变量   初始化大小为 有序数组的前3项之和
+        int closestSum = numArr[0] + numArr[1] + numArr[2];
+
+        // 从0开始，遍历每一个位置
+        for (int currentNumSpot = 0; currentNumSpot < numAmount - 2; currentNumSpot++) {
+            // 初始化 当前位置的两个搭子的位置
+            int middleBroCursor = currentNumSpot + 1;
+            int bigBroCursor = numAmount - 1;
+
+            while (middleBroCursor < bigBroCursor) {
+
+                int sumOfCurrentCombo =
+                        numArr[currentNumSpot] + numArr[middleBroCursor] + numArr[bigBroCursor];
+
+                // 如果 当前三元组的加和 与 目标加和 差的绝对值 小于 ‘当前最接近目标和的三元组的加和’ 与 目标加和 的差的绝对值，
+                // 说明 当前三元组的加和 距离 目标加和更近，
+                if (Math.abs(sumOfCurrentCombo - targetSum) < Math.abs(closestSum - targetSum)) {
+                    // 则：使用它 更新 ‘最接近目标和的加和’
+                    closestSum = sumOfCurrentCombo;
+                }
+
+                // 如果 当前加和 刚好等于 目标和，说明找到了一个解，
+                if (sumOfCurrentCombo == targetSum) {
+                    // 则：直接返回 该加和（等于目标加和）
+                    return targetSum;
+                } else if (sumOfCurrentCombo < targetSum) { // 如果 当前加和 小于 目标和，则：
+                    // 向右移动 左指针 来 得到 更大的加和
+                    middleBroCursor++;
+                } else { // 否则，
+                    // 向左移动右指针 来 得到 更小的和
+                    bigBroCursor--;
+                }
+            }
+        }
+
+        // 返回 ‘最接近目标和’的加和结果
+        return closestSum;
+    }
+}
