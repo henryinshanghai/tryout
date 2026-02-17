82a0ebdc8a21230ba768f3bc68da87cc4c5262e8
HenryInSH <2291972433@qq.com>
2026-02-17 22:56:43 +0800

    --- leetcode209 从给定数组中，找到 满足特定条件（长度最小）的 子数组的长度（滑动窗口）：① 扩展’当前窗口‘（把游标指针作为窗口的右边界）；② 合法的窗口（windowSum >= target）；③ 收缩窗口（while循环 + 逐步向后移动窗口的左边界）； ④ 尝试更新minLength（在while循环中，使用当前窗口的长度取min）；⑤ 返回计算得到的minLength（三目运算符、初始赋值、默认值、计算值）；⑥ 防止加和越界（使用long类型）---

--- src/main/java/com/henry/tryout/leetcodes/Rakuten/min_length_arr_209/exe/Solution_qianwen_2Cursors.java
+++ src/main/java/com/henry/tryout/leetcodes/Rakuten/min_length_arr_209/exe/Solution_qianwen_2Cursors.java
@@ -1,26 +1,31 @@
 package com.henry.tryout.leetcodes.Rakuten.min_length_arr_209.exe;
 
-public class Solution_qianwen {
+public class Solution_qianwen_2Cursors {
     public int minSubArrayLen(int targetSum, int[] numArr) {
         int numAmount = numArr.length;
+
+        // 准备 滑动窗口的左边界
         int subArrLeftBar = 0;
-        long currentSum = 0; // 用 long 防止溢出（可选）
+        // 当前窗口元素的加和值   手段：用long 来 防止溢出（可选）
+        long currentSum = 0;
         int minLength = Integer.MAX_VALUE;
 
-        for (int subArrRightBar = 0; subArrRightBar < numAmount; subArrRightBar++) {
+        for (int currentNumCursor = 0; currentNumCursor < numAmount; currentNumCursor++) {
+            int subArrRightBar = currentNumCursor;
             // 把 当前数组元素 添加到 子序列中
             currentSum += numArr[subArrRightBar];
 
-            // 尝试收缩左边界(可能多次) 来 得到 以当前右边界作为结束元素的、最短的、满足条件的子数组
+            // 尝试 收缩左边界(可能多次) 来 得到 以当前右边界作为结束元素的、最短的、满足条件的 子数组
             while (currentSum >= targetSum) {
-                // 计算 当前子数组的 数组长度，尝试 更新minLength
+                /* 尝试使用 当前’合法子数组‘的 长度 来 更新minLength */
                 minLength = Math.min(minLength, subArrRightBar - subArrLeftBar + 1);
 
+                /* 收缩窗口的左边界 */
                 // 从sum中 移除 左边界元素
                 currentSum -= numArr[subArrLeftBar];
                 // 把 左边界 向右移动一个位置
                 subArrLeftBar++;
-            } /* 退出while循环时，子数组的subArrCurrentSum 已经 小于target，可以继续追加元素了 */
+            } /* 退出while循环时，子数组的subArrCurrentSum 已经 小于target，可以 继续追加元素了 */
         }
 
         // 如果 minLength的值 在for循环后，没有发生变化，说明 压根没有找到 满足条件的子数组，
