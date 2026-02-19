c49d1b3f193e60c8000246a8c695d179c290a584
HenryInSH <2291972433@qq.com>
2026-02-19 20:07:11 +0800

    --- leetcode528 编写一个方法，用于根据‘下标的权重’ 来 决定‘返回该下标的概率’（前缀和数组+upper-bound二分查找）：① 前缀和数组（权重的刻度尺，用于支持随机点的二分查找）；② 生成前缀和数组（容量初始化、元素初始化、元素计算） ③ 创建指定范围内的随机数（Random对象、rand.nextInt(<right_bound>)）；④ 该随机数 所属于的索引（在前缀和数组中 查找 ‘第一个大于该随机数的’ 元素的索引[upper-bound二分查找]）；⑤ upper-bound二分查找（左闭右开区间、left与right指针的初始位置、nums[middle]<=target、返回左指针位置）。 ---

--- src/main/java/com/henry/tryout/leetcodes/Rakuten/possibility_to_pick_an_index_528/exe/Solution_qianwen_prefixSum.java
+++ src/main/java/com/henry/tryout/leetcodes/Rakuten/possibility_to_pick_an_index_528/exe/Solution_qianwen_prefixSum.java
@@ -2,7 +2,7 @@ package com.henry.tryout.leetcodes.Rakuten.possibility_to_pick_an_index_528.exe;
 
 import java.util.Random;
 
-public class Solution_qianwen {
+public class Solution_qianwen_prefixSum {
 
     // w = [1, 3, 2]
     // prefixSum = [1, 4, 6]  → total = 6
@@ -14,11 +14,13 @@ public class Solution_qianwen {
      *
      * @param currentSpotToItsWeightArr 权重数组
      */
-    public Solution_qianwen(int[] currentSpotToItsWeightArr) {
-        // 构造出 其前缀和数组 - 用于 支持对指定索引的快速定位
+    public Solution_qianwen_prefixSum(int[] currentSpotToItsWeightArr) {
+        /* 构造出 其前缀和数组 - 用于 支持对指定索引的快速定位 */
+        // 容量初始化
         currentSpotToItsPrefixSum = new int[currentSpotToItsWeightArr.length];
+        // 按需进行元素初始化
         currentSpotToItsPrefixSum[0] = currentSpotToItsWeightArr[0];
-
+        // 元素赋值
         for (int currentSpot = 1; currentSpot < currentSpotToItsWeightArr.length; currentSpot++) {
             currentSpotToItsPrefixSum[currentSpot] =
                     currentSpotToItsPrefixSum[currentSpot - 1] + currentSpotToItsWeightArr[currentSpot];
@@ -39,24 +41,30 @@ public class Solution_qianwen {
         int randomInt = random.nextInt(maxItemOfPrefixSumArr); // [0, maxItemOfPrefixSumArr)
 
         /* ③ 在 前缀和数组(有序数组) 中 进行 标准的upper-bound(第一个＞ target的元素的位置)的 二分查找 */
-        // 找到第一个 prefixSum[i] > randomInt 的 i
-        int leftBar = 0, rightBar = currentSpotToItsPrefixSum.length; // 标准写法（右边界指向length位置），而不是length-1
+        /* 找到第一个 prefixSum[i] > randomInt 的 i */
+        // 左边界指针 初始指向0
+        int leftBarCursor = 0;
+        // 标准写法：右边界指针 初始指向 length位置，而不是length-1
+        int rightBarCursor = currentSpotToItsPrefixSum.length;
 
         // 当 左边界指针 不小于 右边界指针 时，说明 此时 左指针与右指针 都已经 指向了“bound位置”，则：退出循环
-        while (leftBar < rightBar) { // ① 原则：左闭右开区间的有效性，因此 左边界指针 < 右边界指针(而不是 <=)
-            int middle = (leftBar + rightBar) / 2;
+        while (leftBarCursor < rightBarCursor) { // ① 原则：‘左闭右开区间’的有效性，因此 左边界指针 < 右边界指针(而不是 <=)
+            // 计算出 当前区间的中间位置
+            int middle = (leftBarCursor + rightBarCursor) / 2;
 
-            // 如果 中间位置上的元素 小于等于 该随机数，说明 middle及其左边的元素 不满足条件(>target)，则：
+            // 如果 当前中间位置上的元素 小于等于 该随机数，说明 middle及其左边的元素 不满足条件(>target)，则：
             if (currentSpotToItsPrefixSum[middle] <= randomInt) {
-                // 更新 当前区间的左边界
-                leftBar = middle + 1; // ② 按照区间定义，middle已经查看过了，所以 新区间 从middle+1开始
+                // 按需更新 当前区间的左边界指针
+                // ② 按照区间定义，middle已经查看过了，所以 新区间 从middle+1开始
+                leftBarCursor = middle + 1;
             } else { // 否则，说明 mid 是一个候选答案，但左边 可能还有 更小的合法索引，则：
-                // 更新 当前区间的右边界
-                rightBar = middle; // ③ 按照区间定义，middle作为右边界 不会被包含，所以 可以作为右边界
+                // 按需更新 当前区间的右边界
+                // ③ 按照区间定义，middle作为右边界 不会被包含，所以 可以作为右边界
+                rightBarCursor = middle;
             }
-        }
+        } /* 这种写法，循环结束后，左指针与右指针会指向同一个位置 */
 
         // 返回 左/右边界指针的位置
-        return leftBar;
+        return leftBarCursor;
     }
 }
