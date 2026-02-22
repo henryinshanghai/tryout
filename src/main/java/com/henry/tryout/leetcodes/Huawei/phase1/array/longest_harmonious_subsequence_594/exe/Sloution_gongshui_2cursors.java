9f67a54998fc558bb2e58920d6feb8827d9d995e
HenryInSH <2291972433@qq.com>
2026-02-22 16:09:27 +0800

    --- leetcode594 从给定的数组中，找到其中最长‘和谐子序列’的长度（频次统计法/滑动窗口法）：① 频次统计法（预统计数字的频次、遍历keySet、尝试更新maxSeqLength）；② 滑动窗口法（先排序、扩展窗口右边界、恢复‘窗口合法性’、尝试更新maxSeqLength）；③ 扩展窗口右边界（以游标指针作为右边界）；④ 恢复窗口合法性（向右移动窗口左指针，直到窗口合法）；⑤ 尝试更新maxSeqLength（窗口左右边界数字差值>1时，使用 当前窗口的宽度 尝试更新 maxSeqLength）。 ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase1/array/longest_harmonious_subsequence_594/exe/Sloution_gongshui_2cursors.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase1/array/longest_harmonious_subsequence_594/exe/Sloution_gongshui_2cursors.java
@@ -7,19 +7,31 @@ public class Sloution_gongshui_2cursors {
         Arrays.sort(numArr);
         int numAmount = numArr.length, maxSeqLength = 0;
 
-        for (int leftCursor = 0, rightCursor = 0; rightCursor < numAmount; rightCursor++) {
-            // 当 窗口中的右指针所指向元素 与 左指针所指向元素的 差值 大于1时，说明 当前窗口中的子数组 已经不是 和谐数组，则：
-            while (leftCursor < rightCursor &&
-                    numArr[rightCursor] - numArr[leftCursor] > 1) {
-                // 把 左指针向后移动一个位置 来 收缩窗口
-                leftCursor++;
+        int windowLeftCursor = 0;
+        for (int currentNumCursor = 0; currentNumCursor < numAmount; currentNumCursor++) {
+            /* 扩展窗口的右边界指针 */
+            int windowRightCursor = currentNumCursor;
+
+            /* （扩展右边界后）按需恢复窗口的合法性 */
+            // 当 窗口中的右指针所指向元素 与 左指针所指向元素的 差值 大于1时，
+            // 说明 当前窗口中的子数组 已经不是 和谐数组，
+            while (windowLeftCursor < windowRightCursor &&
+                    numArr[windowRightCursor] - numArr[windowLeftCursor] > 1) {
+                // 则：收缩窗口
+                // 手段：把 左指针 向后移动一个位置；
+                windowLeftCursor++;
             }
-            // 如果 右指针所指向的元素 与 左指针所指向的元素 的差值 等于1，说明 当前窗口的子数组 是一个和谐数组，则：
-            if (numArr[rightCursor] - numArr[leftCursor] == 1) {
-                // 尝试 用当前数组的长度 来 更新最长和谐子序列的长度
-                maxSeqLength = Math.max(maxSeqLength, rightCursor - leftCursor + 1);
+
+            /* （窗口合法后）尝试更新 最长序列长度 */
+            // 如果 右指针所指向的元素 与 左指针所指向的元素 的差值 等于1，
+            // 说明 当前窗口的子数组 是一个和谐数组，
+            if (numArr[windowRightCursor] - numArr[windowLeftCursor] == 1) {
+                // 则：尝试 用当前数组的长度 来 更新最长和谐子序列的长度
+                maxSeqLength = Math.max(maxSeqLength,
+                        windowRightCursor - windowLeftCursor + 1);
             }
         }
+
         return maxSeqLength;
     }
 }
