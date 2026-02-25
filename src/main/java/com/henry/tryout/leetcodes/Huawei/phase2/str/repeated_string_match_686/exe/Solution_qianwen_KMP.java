77ef04bf7ec5488e7695a2db371c19288a2904b8
HenryInSH <2291972433@qq.com>
2026-02-25 23:52:24 +0800

    --- leetcode686 对于给定的字符串a与字符串b，返回 字符串a最少需要重复多少次 才能使字符串b成为 其重复结果的子串（子字符串查找-KMP算法）：① 构造模式字符串的lsp[]数组（前缀指针与子串指针、字符匹配时的处理、字符不匹配时的处理、lsp[i]的赋值）；② KMP算法（准备模式指针与文本指针、字符失配时的处理(模式指针按需回退)、字符匹配时的处理、找到模式串匹配时的处理）；③ 找到模式串匹配时 计算字符串a的重复次数（repeatTimes = (txtCharCursor/len(a) + 1）；④ 获取到文本字符的位置（文本指针%字符串a的长度）；⑤ 循环条件（文本指针<最大文本字符数量）。 ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase2/str/repeated_string_match_686/exe/Solution_qianwen_KMP.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase2/str/repeated_string_match_686/exe/Solution_qianwen_KMP.java
@@ -26,15 +26,17 @@ public class Solution_qianwen_KMP {
         while (currentTxtCharSpot < checkCharMaxAmount) {
             char currentTxtChar = strToRepeat.charAt(currentTxtCharSpot % repeatStrLength);
 
-            // Ⅰ 当 文本字符 与 模式字符 发生了失配时，说明 需要 回退模式字符指针
+            /* Ⅰ 字符失配的情形 */
+            // 当 文本字符 与 模式字符 发生了失配时，说明 需要 回退模式字符指针
             while (currentPatCharSpot > 0 &&
                     currentTxtChar != strToBeSubstring.charAt(currentPatCharSpot)) {
-                // 则：一直回退 模式字符指针 直到 正确位置；
+                // 则：一直回退 模式字符指针 直到 正确位置 来 继续匹配
                 // 回退位置patCursor = lps[patCursor - 1]
                 currentPatCharSpot = currentSpotToItsPrefixSpot[currentPatCharSpot - 1];
             }
 
-            // Ⅱ 如果 字符匹配，说明 应该继续匹配 下一个位置上的 模式字符 与 文本字符，
+            /* Ⅱ 字符失配的情况 */
+            // 如果 字符匹配，说明 应该继续匹配 下一个位置上的 模式字符 与 文本字符，
             if (currentTxtChar == strToBeSubstring.charAt(currentPatCharSpot)) {
                 // 则：把 模式串指针 移动到 下一位置
                 currentPatCharSpot++;
@@ -43,10 +45,8 @@ public class Solution_qianwen_KMP {
             /* Ⅲ 检查是否 完全匹配 */
             // 如果 模式指针 移动到了 模式字符串的末尾，
             if (currentPatCharSpot == substringLength) {
-                // 统共使用了 currentTxtCharSpot+1 个字符（因为currentTxtCharSpot 从 0 开始）
-                // 因此 字符串a 需要重复的次数 = （x + k - 1）/ k [向上取整技巧]
-                int usedTxtCharAmount = currentTxtCharSpot + 1;
-                return (usedTxtCharAmount + repeatStrLength - 1) / repeatStrLength;
+                // 文本字符的数量 / 被重复字符串的字符数量 + 1
+                return (currentTxtCharSpot / repeatStrLength) + 1;
             }
 
             // 把 文本指针 向前前进
@@ -60,31 +60,38 @@ public class Solution_qianwen_KMP {
     // 🐖 这个写法（区别于ConstructLPS） 与 KMP算法的写法 非常类似
     private int[] computeLPS(String patternStr) {
         int patCharAmount = patternStr.length();
-        int[] currentSpotToItsPrefixSpot = new int[patCharAmount];
+        int[] currentSpotToItsPrefixLength = new int[patCharAmount];
 
         // 准备指针
-        int currentPatCharCursor = 1;
-        int currentPrefixCursor = 0;
+        int currentSubArrEndCursor = 1;
+        int currentPrefixEndNextSpotCursor = 0;
 
-        while (currentPatCharCursor < patCharAmount) {
-            /* 检查 字符是否匹配 */
-            // 当 不匹配 时，连续回退模式指针 直到合适位置（或0）
-            while (currentPrefixCursor > 0 &&
-                    patternStr.charAt(currentPatCharCursor) != patternStr.charAt(currentPrefixCursor)) {
-                currentPrefixCursor = currentSpotToItsPrefixSpot[currentPrefixCursor - 1];
+        while (currentSubArrEndCursor < patCharAmount) {
+            /* 字符失配时的情形 */
+            // 当 字符失配 时，连续回退 前缀指针 直到合适位置（或0），然后继续尝试匹配
+            while (currentPrefixEndNextSpotCursor > 0 &&
+                    patternStr.charAt(currentSubArrEndCursor)
+                            != patternStr.charAt(currentPrefixEndNextSpotCursor)) {
+                // 回退到的位置：lps[prefixCursor - 1]
+                currentPrefixEndNextSpotCursor = currentSpotToItsPrefixLength[currentPrefixEndNextSpotCursor - 1];
             }
 
-            // 如果 匹配，则：
-            if (patternStr.charAt(currentPatCharCursor) == patternStr.charAt(currentPrefixCursor)) {
+            /* 字符匹配时的情形 */
+            // 如果 字符匹配，则：
+            if (patternStr.charAt(currentSubArrEndCursor)
+                    == patternStr.charAt(currentPrefixEndNextSpotCursor)) {
                 // 把 前缀指针 向后移动
-                currentPrefixCursor++;
-                currentSpotToItsPrefixSpot[currentPatCharCursor] = currentPrefixCursor;
+                currentPrefixEndNextSpotCursor++;
+                // 为 lps[i] 进行赋值
+                currentSpotToItsPrefixLength[currentSubArrEndCursor] = currentPrefixEndNextSpotCursor;
             }
 
-            // （匹配成功 或者 前缀指针回退到位置0）把模式指针 向后移动一个位置（尝试新的匹配起点）
-            currentPatCharCursor++;
+            /* 不管 前缀指针回退到位置0 或是 字符匹配，子串指针 都要 向后移动一个位置 */
+            // （匹配成功 或者 前缀指针回退到位置0）把 子串字符指针 向后移动一个位置（尝试新的匹配起点）
+            currentSubArrEndCursor++;
         }
 
-        return currentSpotToItsPrefixSpot;
+        // 返回所构造的lps[]数组
+        return currentSpotToItsPrefixLength;
     }
 }
