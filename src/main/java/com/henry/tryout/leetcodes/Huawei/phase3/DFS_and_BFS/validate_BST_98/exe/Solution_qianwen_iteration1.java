8fbd0dcb7f669c5cfef20670823d599f26b5d477
HenryInSH <2291972433@qq.com>
2026-02-26 23:38:14 +0800

    --- leetcode98 校验 给定的二叉树 是不是一棵二叉搜索树BST（迭代法：中序遍历 + 遍历时判断） ：① 中序遍历（栈、当前节点指针、按需移动当前节点指针）；② 准备一个‘序列中的上一个节点(值)’变量（初始为null/Long.MIN_VALUE、存储当前节点(的值)、在后继比较中用作‘前一个节点’）；③ 弹出‘本轮的根’后（比较它与‘上一个节点(的值)’是否存在breach、把它的值记录为‘上一个节点的值’） ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/validate_BST_98/exe/Solution_qianwen_iteration1.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/validate_BST_98/exe/Solution_qianwen_iteration1.java
@@ -1,27 +1,36 @@
 package com.henry.tryout.leetcodes.Huawei.phase3.DFS_and_BFS.validate_BST_98.exe;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 // 对于二叉树节点的遍历，区分 ’访问‘节点的时机 VS. ’处理‘节点的时机
 public class Solution_qianwen_iteration1 {
     public boolean isValidBST(TreeNode currentRootNode) {
-        Stack<TreeNode> stack = new Stack<>();
+        /* 为校验BST而新增的代码👇 */
+        // 准备一个‘前一个节点的值’的变量
+        // 用于存储 遍历过程中的‘当前节点的值’ 来 参与后继比较
         long previousNodeVal = Long.MIN_VALUE;
+        /* 为校验BST而新增的代码👆 */
+
+        // 准备一个栈 用于存储非nil节点
+        Deque<TreeNode> nodeStack = new ArrayDeque<>();
 
         /* 原理：BST的中序遍历序列 必然是 严格单调递增的 */
-        while (currentRootNode != null || !stack.isEmpty()) {
-            /* ① 当 当前节点不为nil节点 时 */
-            // 一路向左，把 搜索路径上 访问到的所有节点 都 添加到栈中
+        while (currentRootNode != null || !nodeStack.isEmpty()) {
+            /* 把 从 当前节点指针所指向的节点 到 最左侧的叶子节点 路径上的 所有节点，都入栈 */
+            // 如果 当前节点为nil节点，就会跳过while，直接执行pop
             while (currentRootNode != null) {
                 // ’访问到的‘当前节点
-                stack.push(currentRootNode);
-                currentRootNode = currentRootNode.left;
+                nodeStack.push(currentRootNode);
+                // 更新 当前节点指针 指向 其左子节点
+                currentRootNode = currentRootNode.left; // 本轮的左
             } /* 循环结束时，栈顶元素 是 最左侧的叶子节点 */
 
-            /* ② 如果 当前节点为nil节点（while循环的退出条件），说明 当前路径搜索结束 */
-            // 弹出 当前的栈顶元素（叶子节点），开始’处理‘它
-            currentRootNode = stack.pop();
+            /* 出栈 最左侧的叶子节点，并 移动 当前节点指针 指向它 */
+            currentRootNode = nodeStack.pop(); // 本轮的根
 
+            /* 为了校验BST而添加的代码（非前序遍历模板代码）👇 */
             // 如果 当前节点的val <= ’上一个访问的节点‘的val，
             // 说明 当前的序列 违反了 BST的约束（中序遍历的结果序列 应为 升序），
             if (currentRootNode.val <= previousNodeVal) {
@@ -31,9 +40,10 @@ public class Solution_qianwen_iteration1 {
 
             // 把 ’当前被处理的节点‘ 记录为 ’序列中的上一个节点‘
             previousNodeVal = currentRootNode.val;
+            /* 为了校验BST而添加的代码（非前序遍历模板代码）👆 */
 
-            // 把 当前节点指针 移动到 其右子节点处（因为已经 处理了 左子节点 与 当前节点了）
-            currentRootNode = currentRootNode.right;
+            /* 把 当前节点指针 移动到 其右子节点处 */
+            currentRootNode = currentRootNode.right; // 本轮的根（当前轮的 左-根-右 结束）
         }
 
         return true;
--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/validate_BST_98/exe/Solution_qianwen_iteration2.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/validate_BST_98/exe/Solution_qianwen_iteration2.java
@@ -2,17 +2,21 @@ package com.henry.tryout.leetcodes.Huawei.phase3.DFS_and_BFS.validate_BST_98.exe
 
 import java.util.Stack;
 
+// Carl提供的写法
 public class Solution_qianwen_iteration2 {
     public boolean isValidBST(TreeNode root) {
         /* 原理：BST的中序遍历 节点结果序列 应该是一个 严格递增的序列 */
         // 手段：在 对树进行中序遍历的过程中，判断 该树是否满足 BST的约束
 
+        /* 为验证BST而新增的代码段👇 */
+        // 与iteration1的区别①：这里使用的是 TreeNode局部变量，而不是integer。
+        TreeNode previousNode = null; // Integer.MIN_VALUE;
+        /* 为验证BST而新增的代码段👆 */
+
         // 准备一个栈，用于 处理节点
         Stack<TreeNode> nodeStack = new Stack<>();
         // 准备 节点指针，用于 访问节点
         TreeNode currentNode = root;
-        // 与iteration1的区别①：这里使用的是 TreeNode局部变量，而不是integer。
-        TreeNode previousNode = null; // Integer.MIN_VALUE;
 
         while (currentNode != null || !nodeStack.isEmpty()) {
             // 与 iteration1的区别②：这里使用了 if/else的写法
@@ -22,7 +26,8 @@ public class Solution_qianwen_iteration2 {
             } else {
                 currentNode = nodeStack.pop(); // 根
 
-                /* 这一段相对于模板代码 是新增的，用于 判断当前处理的节点序列 是否违反了 BST的约束 */
+                /* 这一段相对于模板代码 是新增的，
+                用于 判断当前处理的节点序列 是否违反了 BST的约束 */
                 if (previousNode != null && currentNode.val <= previousNode.val) {
                     return false;
                 }
