0e6cda1b41579aaca59e050727db18f0e4301321
HenryInSH <2291972433@qq.com>
2026-02-26 15:53:02 +0800

    --- leetcode16 从给定的整数序列中 找到3个数字，使它们的加和结果最接近target（定锚指针+游标指针、计算与更新） ：① 对整个序列进行排序（Arrays.sort(<numSeq>)）；② 计算 以当前位置作为起始位置的 三元组的加和结果（三兄弟指针[currNumCursor、middleBroCursor、bigBroCursor]、持续按需调整兄弟指针位置[while循环]、更新‘最接近target的加和结果’）；③ 最接近target的加和结果变量（初始化为nums[0] + nums[1] + nums[2]、每次计算出 curr3BroSum，就尝试更新它）；④ 按需调整兄弟指针位置（比较 curr3BroSum 与 target）； ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/two_cursors/threeSum_closest_16/exe/Solution_qianwen_2Cursors.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/two_cursors/threeSum_closest_16/exe/Solution_qianwen_2Cursors.java
@@ -8,20 +8,22 @@ public class Solution_qianwen_2Cursors {
         Arrays.sort(numArr);
 
         int numAmount = numArr.length;
-        // 准备一个 ‘最接近target的sum’变量   初始化大小为 有序数组的前3项之和
+        // ① 准备一个 ‘最接近target的sum’变量   初始化大小为 有序数组的前3项之和
         int closestSum = numArr[0] + numArr[1] + numArr[2];
 
         // 从0开始，遍历每一个位置
         for (int currentNumSpot = 0; currentNumSpot < numAmount - 2; currentNumSpot++) {
-            // 初始化 当前位置的两个搭子的位置
+            // 初始化 当前位置的两个bro的位置
             int middleBroCursor = currentNumSpot + 1;
             int bigBroCursor = numAmount - 1;
 
+            // 不断按需调整两个bro的位置
             while (middleBroCursor < bigBroCursor) {
-
+                /* ② 计算 当前三兄弟的加和结果 */
                 int sumOfCurrentCombo =
                         numArr[currentNumSpot] + numArr[middleBroCursor] + numArr[bigBroCursor];
 
+                /* ③ 尝试使用 当前加和结果 来 更新‘最接近target的加和结果’ */
                 // 如果 当前三元组的加和 与 目标加和 差的绝对值 小于 ‘当前最接近目标和的三元组的加和’ 与 目标加和 的差的绝对值，
                 // 说明 当前三元组的加和 距离 目标加和更近，
                 if (Math.abs(sumOfCurrentCombo - targetSum) < Math.abs(closestSum - targetSum)) {
@@ -29,6 +31,7 @@ public class Solution_qianwen_2Cursors {
                     closestSum = sumOfCurrentCombo;
                 }
 
+                /* ④ 按需更新兄弟指针的位置 来 继续尝试找到‘最接近target的加和结果’ */
                 // 如果 当前加和 刚好等于 目标和，说明找到了一个解，
                 if (sumOfCurrentCombo == targetSum) {
                     // 则：直接返回 该加和（等于目标加和）
