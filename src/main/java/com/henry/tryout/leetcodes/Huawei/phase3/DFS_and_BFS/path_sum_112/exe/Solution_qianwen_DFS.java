1923e31dee40e4d73d632c6a5d08ae72ec71a823
HenryInSH <2291972433@qq.com>
2026-02-27 13:50:47 +0800

    --- leetcode112 判断 一棵给定的二叉树 中 是否存在 加和结果等于特定值的完整路径（递归法：向下搜索+叶子节点处的检查） ：① 递归终止情形（搜索过程 到达了 nil节点，说明没有搜索到 目标路径，return false;）；② 搜索过程 到达了 叶子节点（检查 currNode.val == currTarget）；③ 搜索过程 到达了 中间节点（继续递归地搜索）；④ 子问题（在子树中，搜索(currTarget-currNode.val)） ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/path_sum_112/exe/Solution_qianwen_DFS.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/path_sum_112/exe/Solution_qianwen_DFS.java
@@ -2,20 +2,24 @@ package com.henry.tryout.leetcodes.Huawei.phase3.DFS_and_BFS.path_sum_112.exe;
 
 public class Solution_qianwen_DFS {
     public boolean hasPathSum(TreeNode currentNode, int currentTargetSum) {
+        /* 情形① 搜索过程 到达了 nil节点 */
         // 如果 当前节点为 null，说明 搜索过程 到达了nil节点，则：
         if (currentNode == null) {
             // 返回 false，表示 不存在 满足条件的路径
             return false;
         }
 
-        // 如果 当前节点的左右子节点 都是null，说明 搜索过程 到达了 叶子节点，则：
+        /* 情形② 搜索过程 到达了 叶子节点 */
+        // 如果 当前节点的左右子节点 都是null，
+        // 说明 搜索过程 到达了 叶子节点，则：
         if (currentNode.left == null &&
                 currentNode.right == null) {
             // 判断 叶子节点的值 与 当前所期待的目标值 是否相等，并 把判断结果 返回给上一级调用
             return currentNode.val == currentTargetSum;
         }
 
-        /* 递归地判断 左子树 或 右子树 中是否存在 满足条件的路径 */
+        /* 情形③ 搜索过程 到达了中间节点 */
+        // 递归地判断 左子树 或 右子树 中是否存在 满足条件的路径
         return hasPathSum(currentNode.left,
                 currentTargetSum - currentNode.val) || // 检查 左子树中是否存在 对应的子路径
                 hasPathSum(currentNode.right,
