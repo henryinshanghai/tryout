6a3c4fdc81007c2cc79a12a67c6d0e4859f06786
HenryInSH <2291972433@qq.com>
2026-02-27 10:19:11 +0800

    --- leetcode98 校验 给定的二叉树 是不是一棵二叉搜索树BST（递归法：中序遍历 + 遍历时判断） ：① 中序遍历（左子树、当前节点、右子树）；② 递归终止条件（当前节点是一个nil节点:return true;）；③ 递归判断左子树（if(!isValidBST(root.left)){}）；④ 判断当前节点（与成员变量prev[记录‘上一个节点’]做比较、把当前节点记录为‘前一个节点’）；⑤ 递归判断右右子树（return isValidBST(root.right);）； ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/validate_BST_98/exe/Solution_qianwen_recursion.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/validate_BST_98/exe/Solution_qianwen_recursion.java
@@ -1,9 +1,13 @@
 package com.henry.tryout.leetcodes.Huawei.phase3.DFS_and_BFS.validate_BST_98.exe;
 
 // 把 二叉树 视为 左子树 + 根节点 + 右子树 的递归结构，
-// 使用 递归的方式 来 进行 节点的中序遍历
+// 使用 递归的方式 来 进行 节点的中序遍历；
 public class Solution_qianwen_recursion {
 //    TreeNode previousNode = null;
+    // 准备一个成员变量     用于记录‘序列中的上一个节点’
+    // 🐖 成员变量 会在 方法的多次调用 之间 保持状态
+    // 因此 如果需要对方法多次调用（比如需要在本地测试多组数据）的话，必须要 每次手动重置 prev
+    // 为了处理这个问题👆，可以把prev 添加为 递归方法的参数（略）
     long previousNodeValInSeq = Long.MIN_VALUE;
 
     public boolean isValidBST(TreeNode currentRootNode) {
