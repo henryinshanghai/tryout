c80d61d6950d22970c0f5bd0d50ce13aa0d2aaf4
HenryInSH <2291972433@qq.com>
2026-02-27 11:25:19 +0800

    --- leetcode101 判断一棵给定的二叉树 是不是 轴对称的（迭代法：查看镜像位置上的两个节点 是否相等） ：① 判断一棵树是否是轴对称（查看镜像位置上的节点对的情况）；② 维护‘镜像位置’上的节点对（使用简单队列Queue、LinkedList、offer、poll）；③ 初始按序添加的节点对（左子节点、右子节点）；④ 节点对的所有情形（左右节点都为nil、左右节点中有一个为nil、左右节点都不是nil）；⑤ 不同情形的处理（跳过后继操作、直接return false、检查val&&按序添加镜像的节点对） ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/symmetric_tree_101/exe/Solution_qianwen_iteration.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/symmetric_tree_101/exe/Solution_qianwen_iteration.java
@@ -21,21 +21,26 @@ public class Solution_qianwen_iteration {
             TreeNode rightOfCurrPair = nodeContainer.poll();
 
             /* 检查 这两个节点 是否相同（互为镜像） */
-            // 两者都空，说明 当前节点对 互为镜像，
+            // ① 当前对中的左节点 与 右节点 都是nil，
+            // 说明 当前节点对 互为镜像，
             if (leftOfCurrPair == null &&
                     rightOfCurrPair == null) {
                 // 则：跳过对 ‘当前节点对’ 的后继检查，直接检查 下一个节点对
                 continue;
             }
 
-            // 如果 不对称（一个空或值不等），说明 当前节点对 不互为镜像，
+            // ② 如果 当前对中的左节点 与 右节点 有一个为nil，
+            // 说明 当前节点对 不互为镜像，
             if (leftOfCurrPair == null ||
-                    rightOfCurrPair == null ||
-                    leftOfCurrPair.val != rightOfCurrPair.val) {
+                    rightOfCurrPair == null) {
                 // 则：返回false 表示 两棵树 不是 互为镜像的
                 return false;
             }
 
+            // ③ 当前对的左节点、右节点 都不为nil
+            if (leftOfCurrPair.val != rightOfCurrPair.val) {
+                return false;
+            }
             /* 把 当前节点对 的左右子节点，以 节点对 添加到 队列中 */
             // ‘外侧’节点对：左子节点的左子节点、右子节点的右子节点
             nodeContainer.offer(leftOfCurrPair.left);
