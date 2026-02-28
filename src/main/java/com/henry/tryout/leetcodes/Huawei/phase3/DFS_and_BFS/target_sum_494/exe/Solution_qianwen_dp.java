2f4eabc71f845c3cca0146df97e75c6a72a5e05b
HenryInSH <2291972433@qq.com>
2026-02-28 21:22:10 +0800

    --- leetcode494 为 给定的整数序列中的每个数字 添加+/- 使得 表达式的结果 为 给定的target的方案数量（计数型01背包：使用有限的物品[每个物品只能选1次] 来 装满背包）：① 转化为’计数型01背包‘问题（’添加上+的数字‘的加和结果 = (sum+target)/2;）；② i与dp[i]的含义（i表示背包容量、dp[i]表示填满该容量背包的方案数量）；③ 递推公式（dp[j] += dp[j-nums[i]]）；④ dp[]元素的初始化（dp[0]=1、填满容量为0的背包有1种方案）；⑤ 遍历顺序（GF：第一遍历物品、BS：第二遍历背包、IR：内层循环倒序遍历）；⑥ 按需返回dp[]元素（dp[bagCapacity]） ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/target_sum_494/exe/Solution_qianwen_dp.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase3/DFS_and_BFS/target_sum_494/exe/Solution_qianwen_dp.java
@@ -7,9 +7,19 @@ public class Solution_qianwen_dp {
             totalSum += currentNum;
         }
 
-        // 边界检查
-        if (Math.abs(targetSum) > totalSum) return 0; // target 太大
-        if ((targetSum + totalSum) % 2 != 0) return 0; // 无法整除
+        /* 边界检查 */
+        // 如果 target 比起 非负整数序列 所有元素都选择+得到的结果 还要打，
+        // 说明 不可能存在任何可行方案，
+        if (Math.abs(targetSum) > totalSum) {
+            // 则：返回0
+            return 0;
+        }
+        // 如果 (target + totalSum) 无法被2整除，
+        // 说明 我们想要凑出的数 不是一个整数，不可能存在任何可行方案（因为所有的物品都是整数）
+        if ((targetSum + totalSum) % 2 != 0) {
+            // 则：返回0
+            return 0;
+        }
 
         // 问题的等价转化
         /*
@@ -18,26 +28,35 @@ public class Solution_qianwen_dp {
                 abs(addPlus_sum) - abs(addMinus_sum) = target;
             => abs(addPlus_sum) = (target + sum) / 2;
          */
-        // 原问题 ⇨ 0-1背包问题：使用 特定的物品选项(只能选择1次) 来 装满 容量为bagCapacity的背包，最多有 多少种装法？
+        // 原问题 ⇨ 0-1背包问题：
+        // 使用 特定的物品选项(只能选择1次) 来 装满/凑出 容量为bagCapacity的背包，最多有 多少种装法？
         int bagCapacity = (targetSum + totalSum) / 2;
-        if (bagCapacity < 0) return 0; // bagCapacity 不能为负
+        // 如果 背包容量 小于0，说明 不存在任何可行方案
+        if (bagCapacity < 0) {
+            // 则：返回0
+            return 0;
+        }
 
-        // currentSumToItsSchemaAmount[j]：得到加和结果j 有dp[j]种选择方案；
-        int[] currentSumToItsSchemaAmount = new int[bagCapacity + 1];
-        currentSumToItsSchemaAmount[0] = 1; // 空集
+        /* 下标i 与 数组dp[i]的定义 */
+        // currentCapacityToItsSchemeAmount[j]：得到 加和结果j 存在有 dp[j]种选择方案；
+        int[] currentCapacityToItsSchemeAmount = new int[bagCapacity + 1];
 
-        // 0-1背包的写法：① 先物品，② 后背包，③ 内层循环倒序
+        /* dp[]数组元素的按需初始化 */
+        currentCapacityToItsSchemeAmount[0] = 1; // 空集
+
+        /* 正确地计算递推公式，得到所有的dp[i]元素值 */
+        // 0-1背包的写法：① 先物品(Goods First)，② 后背包(Bag Second)，③ 内层循环倒序(Inner loop Reverse)
         for (int currentNum : numArr) {
             for (int currentSum = bagCapacity; currentSum >= currentNum; currentSum--) { // 背包容量递减
                 /* “组合类0-1背包问题”的一般递推公式：dp[j] += dp[j-nums[i]] */
                 // dp[j-nums[i]] 表示 刨掉nums[i]的空间 时，会有 多少种方案 填满 剩余空间。
                 // 填满 空间j的方案数量 = 所有 刨掉当前元素后的 填满方案数量 相累加。
-                currentSumToItsSchemaAmount[currentSum] +=
-                        currentSumToItsSchemaAmount[currentSum - currentNum];
+                currentCapacityToItsSchemeAmount[currentSum] +=
+                        currentCapacityToItsSchemeAmount[currentSum - currentNum];
             }
         }
 
         // 从dp[]数组中 返回满足题意的元素
-        return currentSumToItsSchemaAmount[bagCapacity];
+        return currentCapacityToItsSchemeAmount[bagCapacity];
     }
 }
