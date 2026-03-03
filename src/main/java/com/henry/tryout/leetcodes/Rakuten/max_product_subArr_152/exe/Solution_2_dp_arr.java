2aa698985e9d4b42ec10b23ea9ce5c976d3b41d0
HenryInSH <2291972433@qq.com>
2026-03-03 22:14:57 +0800

    --- leetcode152 从元素序列（包含负数）中找到乘积最大的子数组（动态规划）：① 为每个位置维护 以其作为结束位置的 子数组的最大乘积、最小乘积（因为 乘入一个负数 会反转 当前最大乘积与当前最小乘积）；② 计算当前位置的最大乘积、最小乘积（如果当前元素是负数 则交换 最大乘积与最小乘积、维护最大乘积[要么使用当前元素 扩展当前子数组，要么使用当前元素 重开子数组]、维护最小乘积[同理]）；③ 维护全局的最大乘积（max(currMaxProduct, globalMaxProduct);）；④ 全局最大乘积的初始化（nums[0]） ---

--- src/main/java/com/henry/tryout/leetcodes/Rakuten/max_product_subArr_152/exe/Solution_2_dp_arr.java
+++ src/main/java/com/henry/tryout/leetcodes/Rakuten/max_product_subArr_152/exe/Solution_2_dp_arr.java
@@ -6,44 +6,49 @@ public class Solution_2_dp_arr {
 
         /* 准备dp[]数组 */
         // i：元素位置；dp1[i]:以i作为结束位置的子数组的最大乘积
-        int[] currentSpotToMaxProductEndWithIt = new int[numAmount];
+        int[] currSpotToMaxProductOfSubArrEndWithIt = new int[numAmount];
         // i：元素位置；dp1[i]:以i作为结束位置的子数组的最小乘积
-        int[] currentSpotToMinProductEndWithIt = new int[numAmount];
+        // 原因：因为 负数元素 乘入当前乘积后，会反转 当前的最大乘积 与 最小乘积
+        int[] currSpotToMinProductOfSubArrEndWithIt = new int[numAmount];
 
         /* dp[]元素的初始化 */
-        currentSpotToMaxProductEndWithIt[0] = nums[0];
-        currentSpotToMinProductEndWithIt[0] = nums[0];
+        currSpotToMaxProductOfSubArrEndWithIt[0] = nums[0];
+        currSpotToMinProductOfSubArrEndWithIt[0] = nums[0];
 
         // 准备一个变量   用于维护‘乘积最大的子数组’的最大乘积
-        int maxProduct = nums[0];
+        // 🐖 初始值 需要设置为 当前子数组的结果（因为 如果没能找到 更大的乘积，它会 被直接返回）
+        int maxProductOfAllSubArr = nums[0];
 
         /* 遍历顺序 */
         for (int currentSpot = 1; currentSpot < nums.length; currentSpot++) {
             int currentNum = nums[currentSpot];
             /* 在当前元素为负数时，交换 maxProduct 与 minProduct */
             if (currentNum < 0) {
-                int temp = currentSpotToMaxProductEndWithIt[currentSpot - 1];
-                currentSpotToMaxProductEndWithIt[currentSpot - 1] = currentSpotToMinProductEndWithIt[currentSpot - 1];
-                currentSpotToMinProductEndWithIt[currentSpot - 1] = temp;
+                int temp = currSpotToMaxProductOfSubArrEndWithIt[currentSpot - 1];
+                currSpotToMaxProductOfSubArrEndWithIt[currentSpot - 1] = currSpotToMinProductOfSubArrEndWithIt[currentSpot - 1];
+                currSpotToMinProductOfSubArrEndWithIt[currentSpot - 1] = temp;
             }
 
             // 维护 ‘以i作为结束位置的子数组’的最大乘积
-            currentSpotToMaxProductEndWithIt[currentSpot] = Math.max(
-                    currentSpotToMaxProductEndWithIt[currentSpot - 1] * currentNum, // option1 乘进来 当前元素
-                    currentNum // option2 从当前元素 重开子数组
+            currSpotToMaxProductOfSubArrEndWithIt[currentSpot] = Math.max(
+                    currSpotToMaxProductOfSubArrEndWithIt[currentSpot - 1]
+                            * currentNum, // option1 使用 当前元素 来 扩展当前子数组
+                    currentNum // option2 使用 当前元素 来 重开当前子数组
             );
 
             // 维护 ‘以i作为结束位置的子数组’的最小乘积
-            currentSpotToMinProductEndWithIt[currentSpot] = Math.min(
-                    currentSpotToMinProductEndWithIt[currentSpot - 1] * currentNum, // option1 乘进来 当前元素
-                    currentNum // option2 从当前元素 重开子数组
+            currSpotToMinProductOfSubArrEndWithIt[currentSpot] = Math.min(
+                    currSpotToMinProductOfSubArrEndWithIt[currentSpot - 1]
+                            * currentNum, // option1 使用 当前元素 来 扩展当前子数组
+                    currentNum // option2 使用 当前元素 来 重开当前子数组
             );
 
             // 维护 ‘最大乘积’
-            maxProduct = Math.max(maxProduct, currentSpotToMaxProductEndWithIt[currentSpot]);
+            maxProductOfAllSubArr =
+                    Math.max(maxProductOfAllSubArr, currSpotToMaxProductOfSubArrEndWithIt[currentSpot]);
         }
 
         // 返回‘最大乘积’
-        return maxProduct;
+        return maxProductOfAllSubArr;
     }
 }
