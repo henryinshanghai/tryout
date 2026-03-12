f991ccd23b8596c5d1454fe857f8664ec799fe2d
HenryInSH <2291972433@qq.com>
2026-03-12 10:28:37 +0800

    --- 简单方法的字节码指令解释：① 查看字节码的手段（View | show bytecode with jclasslib）； ② 赋值语句（先把 常量 压入操作数栈、再把 栈顶数值 存入 局部变量表的索引x处）；③ 运算&赋值语句（先从局部变量表中 把局部变量加载到栈顶，再进行运算，最后把运算结果加载）；④ 返回语句（先把要返回的变量 加载到 栈顶，再 把栈顶元素 返回给调用者）。---

--- src/main/java/com/henry/tryout/easy_coding/jvm_04/memory_layout_02/thread_private_02/JVM_stack_01/exe/ViewOperationStack_01.java
+++ src/main/java/com/henry/tryout/easy_coding/jvm_04/memory_layout_02/thread_private_02/JVM_stack_01/exe/ViewOperationStack_01.java
@@ -1,8 +1,9 @@
-package com.henry.tryout.easy_coding.jvm_04.memory_layout_02.thread_private_02.JVM_stack;
+package com.henry.tryout.easy_coding.jvm_04.memory_layout_02.thread_private_02.JVM_stack_01.exe;
 
 // 验证：JVM虚拟机栈是怎样实现一个方法的调用的
 // 手段：查看方法调用时，实际执行的字节码指令 - 工具：jclasslib
-// 没啥卵用 看不懂~
+// LOAD指令：把 局部变量表中的变量 加载到 操作数栈中
+// STORE指令：把 操作栈栈顶的数 存储到 局部变量表的对应位置上
 public class ViewOperationStack_01 {
     public static void main(String[] args) {
         Test test = new Test();
@@ -13,13 +14,10 @@ public class ViewOperationStack_01 {
 
 class Test {
     public int simpleMethod() {
-        // 加载(LOAD)局部变量表中的局部变量到 方法帧的操作栈中
         int x = 13;
         int y = 14;
-        // 在操作栈中执行运算，并把运算结果 写回到(STORE)局部变量表中
         int z = x + y;
 
-        // 返回栈顶元素
         return z;
     }
 }
