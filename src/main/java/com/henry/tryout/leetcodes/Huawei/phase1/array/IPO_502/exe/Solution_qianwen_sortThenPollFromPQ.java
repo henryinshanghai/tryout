271409c9847f58d0cd261800b492917c50f5c9d4
HenryInSH <2291972433@qq.com>
2026-03-21 18:11:51 +0800

    --- leetcode502 计算从n个项目中，选择投资k个项目 所能得到的最大净余额（索引数组 + 最大队列）：① 按照所需投资从小到大地排序项目编号（索引数组）；② 获取并记录 当前轮投资 所能投资的所有项目的利润（排序后的项目编号数组、while循环、最大优先队列）；③ 获取并累计 本轮投资所能得到的最大利润（currNetBalance += maxQueue.poll()）；④ 朴素的lambda表达式结构（(a, b) -> b - a） ---

--- src/main/java/com/henry/tryout/leetcodes/Huawei/phase1/array/IPO_502/exe/Solution_qianwen_sortThenPollFromPQ.java
+++ src/main/java/com/henry/tryout/leetcodes/Huawei/phase1/array/IPO_502/exe/Solution_qianwen_sortThenPollFromPQ.java
@@ -30,18 +30,18 @@ public class Solution_qianwen_sortThenPollFromPQ {
         // 最多进行 k轮投资 - 每轮投资都 贪心地 获取最大利润
         for (int currentRound = 0; currentRound < maxAllowedProjectAmount; currentRound++) {
             /* 步骤2：计算出 本轮投资 所能得到的最大利润 */
-            // 手段：获取到 本轮投资 所有能够投资的项目 的利润，取其中的最大利润（使用最大队列）。
+            // 手段：获取到 本轮投资 所有能够投资的所有项目 的利润，取其中的最大利润（使用最大队列）。
             while (currentProjectCursor < allProjectAmount && // 条件① 项目指针 在允许范围内
                     projectNoToItsCost[projectNoArrSortByCostInAsc[currentProjectCursor]] <= currentNetBalance) { // 条件② 手上的净余额 超过 当前所选的项目的成本
-                // 获取到 当前所选项目的利润
-                int currentSelectedProjectsProfit = projectNoToItsProfit[projectNoArrSortByCostInAsc[currentProjectCursor]];
+                // 获取到 当前可选择项目的利润
+                int currentSelectableProjectsProfit = projectNoToItsProfit[projectNoArrSortByCostInAsc[currentProjectCursor]];
                 // 把 该项目的利润 作为队列元素 添加到 优先队列中    用于 后继快速获取到 所有可选项目中 利润最大的项目
-                projectProfitMaxQueue.offer(currentSelectedProjectsProfit);
-                // 把 项目指针 向后移动一个位置  用于继续查看 下一个项目 是不是 也是可选的
+                projectProfitMaxQueue.offer(currentSelectableProjectsProfit);
+                // 把 项目指针 向后移动一个位置  用于继续检查 下一个所需投资最小的项目 是不是 也是可选择的
                 currentProjectCursor++;
             }
 
-            // 如果 当前 已经没有 任何”可启动的项目“，说明 无法继续投资了，
+            // 如果 当前 已经没有 任何”可投资的项目“，说明 无法继续投资了，
             if (projectProfitMaxQueue.isEmpty()) {
                 // 则：提前结束   防止poll()时引发NPE
                 break;
